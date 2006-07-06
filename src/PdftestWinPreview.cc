0870e9aec56bb9f7bf0c6aae6196b930dec1620a
kjk <kjk@841c7e00-0c1d-0410-a694-8ba26473ccb1>
2006-07-06 02:14:13 +0000

    move windows preview code to a separate file

---
+++ src/PdftestWinPreview.cc
@@ -0,0 +1,224 @@
+#include <assert.h>
+#include <windows.h>
+#include "SplashBitmap.h"
+
+#define WIN_CLASS_NAME  "PDFTEST_PDF_WIN"
+#define COL_WINDOW_BG RGB(0xff, 0xff, 0xff)
+
+static HWND             gHwnd = NULL;
+static HBRUSH           gBrushBg;
+static BITMAPINFO *     gDibInfo = NULL;
+static SplashBitmap *   gCurrBitmap = NULL;
+static int              gBitmapDx = -1;
+static int              gBitmapDy = -1;
+
+int RectDx(RECT *r)
+{
+    int dx = r->right - r->left;
+    assert(dx >= 0);
+    return dx;
+}
+
+int RectDy(RECT *r)
+{
+    int dy = r->bottom - r->top;
+    assert(dy >= 0);
+    return dy;
+}
+
+/* Set the client area size of the window 'hwnd' to 'dx'/'dy'. */
+void WinResizeClientArea(HWND hwnd, int dx, int dy)
+{
+    RECT rc;
+    RECT rw;
+    int  win_dx, win_dy;
+
+    GetClientRect(hwnd, &rc);
+    if ((RectDx(&rc) == dx) && (RectDy(&rc) == dy))
+        return;
+    GetWindowRect(hwnd, &rw);
+    win_dx = RectDx(&rw) + (dx - RectDx(&rc));
+    win_dy = RectDy(&rw) + (dy - RectDy(&rc));
+    SetWindowPos(hwnd, NULL, 0, 0, win_dx, win_dy, SWP_NOACTIVATE | SWP_NOREPOSITION | SWP_NOZORDER);
+}
+
+void OnPaint(HWND hwnd)
+{
+    HDC             hdc;
+    PAINTSTRUCT     ps;
+    RECT            rc;
+    int             bmpRowSize;
+    SplashColorPtr  bmpData;
+
+    hdc = BeginPaint(hwnd, &ps);
+    SetBkMode(hdc, TRANSPARENT);
+    GetClientRect(hwnd, &rc);
+
+    bmpRowSize = gCurrBitmap->getRowSize();
+    bmpData = gCurrBitmap->getDataPtr();
+
+    gDibInfo->bmiHeader.biWidth = gBitmapDx;
+    gDibInfo->bmiHeader.biHeight = -gBitmapDy;
+    gDibInfo->bmiHeader.biSizeImage = gBitmapDy * bmpRowSize;
+
+    FillRect(hdc, &ps.rcPaint, gBrushBg);
+    DrawText (hdc, "Hello world", -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER) ;
+
+    SetDIBitsToDevice(hdc,
+        0, /* destx */
+        0, /* desty */
+        gBitmapDx, /* destw */
+        gBitmapDy, /* desth */
+        0, /* srcx */
+        0, /* srcy */
+        0, /* startscan */
+        gBitmapDy, /* numscans */
+        bmpData, /* pBits */
+        gDibInfo, /* pInfo */
+        DIB_RGB_COLORS /* color use flag */
+    );
+
+    EndPaint(hwnd, &ps);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+        case WM_CREATE:
+            // do nothing
+            break;
+
+        case WM_ERASEBKGND:
+            return TRUE;
+
+        case WM_PAINT:
+            /* it might happen that we get WM_PAINT after destroying a window */
+            OnPaint(hwnd);
+            break;
+
+        case WM_DESTROY:
+            /* WM_DESTROY might be sent as a result of File\Close, in which case CloseWindow() has already been called */
+            break;
+
+        default:
+            return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+BOOL RegisterWinClass(void)
+{
+    WNDCLASSEX  wcex;
+    ATOM        atom;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = NULL;
+    wcex.hIcon          = NULL;
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = NULL;
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = WIN_CLASS_NAME;
+    wcex.hIconSm        = NULL;
+
+    atom = RegisterClassEx(&wcex);
+    if (atom)
+        return TRUE;
+    return FALSE;
+}
+
+int InitWinIfNecessary(void)
+{
+    if (gHwnd)
+        return TRUE;
+
+    if (!RegisterWinClass())
+        return FALSE;
+
+    gBrushBg = CreateSolidBrush(COL_WINDOW_BG);
+
+    gHwnd = CreateWindow(
+        WIN_CLASS_NAME, "",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0,
+        CW_USEDEFAULT, 0,
+        NULL, NULL,
+        NULL, NULL);
+
+    if (!gHwnd)
+        return FALSE;
+
+    assert(!gDibInfo);
+    gDibInfo = (BITMAPINFO*)malloc(sizeof(BITMAPINFO) + 12);
+    if (!gDibInfo)
+        return FALSE;
+    gDibInfo->bmiHeader.biSize = sizeof(gDibInfo->bmiHeader);
+    gDibInfo->bmiHeader.biPlanes = 1;
+    gDibInfo->bmiHeader.biBitCount = 24;
+    gDibInfo->bmiHeader.biCompression = BI_RGB;
+    gDibInfo->bmiHeader.biXPelsPerMeter = 2834;
+    gDibInfo->bmiHeader.biYPelsPerMeter = 2834;
+    gDibInfo->bmiHeader.biClrUsed = 0;
+    gDibInfo->bmiHeader.biClrImportant = 0;
+
+    ShowWindow(gHwnd, SW_SHOW);
+    return TRUE;
+}
+
+void PumpMessages(void)
+{
+    BOOL    isMessage;
+    MSG     msg;
+
+    for (;;) {
+        isMessage = PeekMessage(&msg, NULL, 0, 0, PM_REMOVE);
+        if (!isMessage)
+            return;
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+void PreviewBitmap_Init(void)
+{
+    /* no need to do anything */
+}
+
+void PreviewBitmap_Destroy(void)
+{
+    PostQuitMessage(0);
+    PumpMessages();
+    free(gDibInfo);
+    gDibInfo = NULL;
+    DeleteObject(gBrushBg);
+}
+
+void PreviewBitmap(SplashBitmap *bitmap)
+{
+    assert(bitmap);
+    if (!bitmap)
+        return;
+    if (!InitWinIfNecessary())
+        return;
+
+    gCurrBitmap = bitmap;
+    if ( (bitmap->getWidth() != gBitmapDx) ||
+        (bitmap->getHeight() != gBitmapDy) ) {
+        gBitmapDx = bitmap->getWidth();
+        gBitmapDy = bitmap->getHeight();
+        WinResizeClientArea(gHwnd, gBitmapDx, gBitmapDy);
+    }
+    assert(gHwnd);
+
+    InvalidateRect(gHwnd, NULL, FALSE);
+    UpdateWindow(gHwnd);
+
+    PumpMessages();
+}
+
+
