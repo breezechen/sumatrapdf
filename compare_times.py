e38a8ca798dd98a67c4920983ac3c948e9f04374
kjk <kjk@841c7e00-0c1d-0410-a694-8ba26473ccb1>
2006-09-01 07:30:05 +0000

    adding script for comparing timings of different versions of the code

---
+++ compare_times.py
@@ -0,0 +1,156 @@
+import sys, os, os.path, string
+
+MAX_PERCENT_DIFF = 25.0
+
+def uage_and_exit():
+  print "Usage: compare_times.py <stats-file> <stats-file>"
+  sys.exit(1)
+
+# return difference between <one> and <two> in percent
+def percent_diff(one, two):
+  return 100.0 - (two * float(100) / one)
+
+def percent_diff_abs(one, two):
+  return abs(percent_diff(one, two))
+
+def get_time(txt):
+  t = txt.split()
+  assert 2 == len(t)
+  assert "ms" == t[1]
+  return float(t[0])
+
+# given text in the form "page N", return N as an int
+def get_page_no(txt):
+  lp = txt.split()
+  assert "page" == lp[0]
+  assert 2 == len(lp)
+  return int(lp[1])
+
+class Stats:
+  def __init__(self, file_name):
+    self.file_name = file_name
+    self.page_count = None
+    self.timings = []
+    self.load_time = None
+
+def verify_stats(stats):
+  if 0 == len(stats): return
+  file_name = stats[0].file_name
+  count = len(stats[0].timings)
+  assert count == stats[0].page_count
+  for stat in stats:
+    assert None != stat.page_count
+    assert None != stat.load_time
+    assert file_name == stat.file_name
+    assert count == len(stat.timings)
+    assert count == stat.page_count
+
+def get_load_avg_time(results):
+  total_time = 0.0
+  for res in results:
+    total_time += res.load_time
+  avg = total_time / float(len(results))
+  count = 0
+  total_time = 0.0
+  for res in results:
+    percent_diff = percent_diff_abs(res.load_time, avg)
+    if percent_diff > MAX_PERCENT_DIFF:
+      #print "%.2f %.2f %.2f" % (res.load_time, avg, percent_diff)
+      continue
+    total_time += res.load_time
+    count += 1
+  avg = total_time / float(count)
+  return avg
+
+def get_page_avg_time(stats, page_no):
+  total_time = 0.0
+  #print page_no
+  for stat in stats:
+    #print stat.timings
+    total_time += stat.timings[page_no]
+  avg = total_time / float(len(stats))
+  count = 0
+  total_time = 0.0
+  for stat in stats:
+    percent_diff = percent_diff_abs(stat.timings[page_no], avg)
+    if percent_diff > MAX_PERCENT_DIFF:
+      #print "%.2f %.2f %.2f" % (stat.timings[page_no], avg, percent_diff)
+      continue
+    total_time += stat.timings[page_no]
+    count += 1
+  avg = total_time / float(count)
+  return avg
+
+def parse_stats(file_name):
+  stats = []
+  curStat = None  
+  fo = open(file_name, "rb")
+  for l in fo:
+    l = l.strip()
+    #print l
+    lp = l.split(":", 1)
+    assert 2 == len(lp)
+    key = lp[0]
+    if "started" == key:
+      assert None == curStat
+      curStat = Stats(lp[1])
+      continue
+    if "finished" == key:
+      if lp[1] != curStat.file_name:
+        print "file names mismatch %s %s" % (curStat.file_name, lp[1])
+      stats.append(curStat)
+      curStat = None
+      continue
+    if "load" == key:
+      curStat.load_time = get_time(lp[1])
+      continue
+    if "page count" == key:
+      curStat.page_count = int(lp[1])
+      continue
+    if key.startswith("page"):
+      pageNo = get_page_no(key)
+      assert pageNo == (len(curStat.timings) + 1)
+      curStat.timings.append(get_time(lp[1]))
+      continue
+
+  fo.close()
+  return stats
+
+def calc_avg(stats):
+  avg = Stats(stats[0].file_name)
+  avg.timings = []
+  avg.page_count = stats[0].page_count
+  avg.load_time = get_load_avg_time(stats)
+  for page_no in range(avg.page_count):
+    avg.timings.append(get_page_avg_time(stats, page_no))
+  return avg
+
+def dump_stats(file_one, avg_one, file_two, avg_two):
+  print file_one + ", " + file_two
+  one = avg_one.load_time
+  two = avg_two.load_time
+  d = percent_diff_abs(one, two)
+  print "loading time, %.2f, %.2f, %%%.2f" % (one, two, d)
+  assert avg_one.page_count == avg_two.page_count
+  for page_no in range(avg_one.page_count):
+    one = avg_one.timings[page_no]
+    two = avg_two.timings[page_no]
+    d = percent_diff_abs(one, two)
+    print "page %d, %.2f, %.2f, %%%.2f" % (page_no+1, one, two, d)
+
+def compare_stats(file_one, file_two):
+  stats_one = parse_stats(file_one)
+  verify_stats(stats_one)
+  avg_one = calc_avg(stats_one)
+  stats_two = parse_stats(file_two)  
+  verify_stats(stats_two)
+  avg_two = calc_avg(stats_two)
+  dump_stats(file_one, avg_one, file_two, avg_two)
+
+def main():
+  if 3 != len(sys.argv):
+    usage_and_exit()
+  compare_stats(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+  main()
