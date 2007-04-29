0eb140c95495c6df92af1e4e998a4a7340eeba3a
kjk <kjk@841c7e00-0c1d-0410-a694-8ba26473ccb1>
2007-04-29 07:20:23 +0000

    update version to 0.6

--- src/SumatraPDF.cpp
+++ src/SumatraPDF.cpp
@@ -1983,7 +1983,7 @@ static void WindowInfo_Paint(WindowInfo *win, HDC hdc, PAINTSTRUCT *ps)
 #define SUMATRA_TXT             "Sumatra PDF"
 #define SUMATRA_TXT_FONT        "Arial Black"
 #define SUMATRA_TXT_FONT_SIZE   24
-#define BETA_TXT                "Beta v0.5"
+#define BETA_TXT                "Beta v0.6"
 #define BETA_TXT_FONT           "Arial Black"
 #define BETA_TXT_FONT_SIZE      12
 #define LEFT_TXT_FONT           "Arial"
