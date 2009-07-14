breezechen/sumatrapdf

commit 24fcf62bb6a7915d1cd0b9eafbe76c30c6e4e1bb

merge latest mupdf changes
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@1293 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/mupdf/pdf_open.c
@@ -712,6 +712,13 @@ pdf_loadxref2(pdf_xref *xref)
 		goto cleanup;
 	}
 
+	error = pdf_getpagecount(xref, &xref->pagecount);
+	if (error)
+	{
+		error = fz_rethrow(error, "cannot determine page count");
+		goto cleanup;
+	}
+
 	return fz_okay;
 
 cleanup:

mupdf/mupdf/pdf_repair.c
@@ -346,6 +346,10 @@ pdf_repairxref2(pdf_xref *xref, fz_stream *file)
 		}
 	}
 
+	error = pdf_getpagecount(xref, &xref->pagecount);
+	if (error)
+		return fz_rethrow(error, "cannot determine page count");
+
 	fz_free(list);
 	return fz_okay;
 

src/PdfEngine.cc
@@ -144,7 +144,6 @@ PdfEngine::PdfEngine() :
         , _pageCount(INVALID_PAGE_NO) 
         , _xref(NULL)
         , _outline(NULL)
-        , _pageTree(NULL)
         , _pages(NULL)
         , _rast(NULL)
 {
@@ -160,9 +159,6 @@ PdfEngine::~PdfEngine()
         }
         free(_pages);
     }
-
-    if (_pageTree)
-        pdf_droppagetree(_pageTree);
 
     if (_outline)
         pdf_dropoutline(_outline);
@@ -225,10 +221,6 @@ bool PdfEngine::load(const TCHAR *fileName, WindowInfo *win, bool tryrepair)
     }
 
 DecryptedOk:
-    error = pdf_loadpagetree(&_pageTree, _xref);
-    if (error)
-        goto Error;
-
     /*
      * Load meta information
      * TODO: move this into mupdf library
@@ -257,7 +249,7 @@ DecryptedOk:
     // error might prevent loading some pdfs that would
     // otherwise get displayed
 
-    _pageCount = _pageTree->count;
+    _pageCount = _xref->pagecount;
     _pages = (pdf_page**)malloc(sizeof(pdf_page*) * _pageCount);
     for (int i = 0; i < _pageCount; i++)
         _pages[i] = NULL;
@@ -307,7 +299,10 @@ int PdfEngine::findPageNo(fz_obj *dest)
 
     for (p = 0; p < _pageCount; p++)
     {
-        fz_obj *page = _pageTree->pref[p];
+	fz_obj *page;
+	fz_error error = pdf_getpageobject(_xref, p, &page);
+	if (error)
+	    continue;
         int np = fz_tonum(page);
         int gp = fz_togen(page);
         if (n == np && g == gp)
@@ -335,9 +330,11 @@ pdf_page *PdfEngine::getPdfPage(int pageNo)
             DBG_OUT("Fitz: ReleaseSemaphore error!\n");
         return page;
     }
-    // TODO: should check for error from pdf_getpageobject?
-    fz_obj * obj = pdf_getpageobject(_pageTree, pageNo - 1);
-    fz_error error = pdf_loadpage(&page, _xref, obj);
+    fz_obj * obj;
+    fz_error error = pdf_getpageobject(_xref, pageNo -1, &obj);
+    if (!error) {
+        error = pdf_loadpage(&page, _xref, obj);
+    }
     if (error) {
         if (!ReleaseSemaphore(_getPageSem, 1, NULL))
             DBG_OUT("Fitz: ReleaseSemaphore error!\n");
@@ -363,20 +360,24 @@ void PdfEngine::dropPdfPage(int pageNo)
 int PdfEngine::pageRotation(int pageNo)
 {
     assert(validPageNo(pageNo));
-    fz_obj *dict = pdf_getpageobject(pages(), pageNo - 1);
-    int rotation;
-    fz_error error = pdf_getpageinfo(_xref, dict, NULL, &rotation);
-    if (error)
-        return INVALID_ROTATION;
+    fz_obj *page;
+    int rotation = INVALID_ROTATION;
+    fz_error error = pdf_getpageobject(_xref, pageNo - 1, &page);
+    if (!error) {
+	fz_error error = pdf_getpageinfo(_xref, page, NULL, &rotation);
+    }
     return rotation;
 }
 
 SizeD PdfEngine::pageSize(int pageNo)
 {
     assert(validPageNo(pageNo));
-    fz_obj *dict = pdf_getpageobject(pages(), pageNo - 1);
+    fz_obj *page;
     fz_rect bbox;
-    fz_error error = pdf_getpageinfo(_xref, dict, &bbox, NULL);
+    fz_error error = pdf_getpageobject(_xref, pageNo - 1, &page);
+    if (!error) {
+	error = pdf_getpageinfo(_xref, page, &bbox, NULL);
+    }
     if (error)
         return SizeD(0,0);
     return SizeD(fabs(bbox.x1 - bbox.x0), fabs(bbox.y1 - bbox.y0));

mupdf/apps/common/pdfapp.c
@@ -114,14 +114,6 @@ void pdfapp_open(pdfapp_t *app, char *filename)
 	}
 
 	/*
-	 * Load page tree
-	 */
-
-	error = pdf_loadpagetree(&app->pages, app->xref);
-	if (error)
-		pdfapp_error(app, error);
-
-	/*
 	 * Load meta information
 	 * TODO: move this into mupdf library
 	 */
@@ -167,6 +159,7 @@ void pdfapp_open(pdfapp_t *app, char *filename)
 	 * Start at first page
 	 */
 
+	app->pagecount = app->xref->pagecount;
 	app->shrinkwrap = 1;
 	if (app->pageno < 1)
 		app->pageno = 1;
@@ -183,10 +176,6 @@ void pdfapp_open(pdfapp_t *app, char *filename)
 
 void pdfapp_close(pdfapp_t *app)
 {
-	if (app->pages)
-		pdf_droppagetree(app->pages);
-	app->pages = nil;
-
 	if (app->page)
 		pdf_droppage(app->page);
 	app->page = nil;
@@ -257,14 +246,16 @@ static void pdfapp_showpage(pdfapp_t *app, int loadpage, int drawpage)
 			pdf_droppage(app->page);
 		app->page = nil;
 
-		obj = pdf_getpageobject(app->pages, app->pageno - 1);
+		error = pdf_getpageobject(app->xref, app->pageno, &obj);
+		if (error)
+			pdfapp_error(app, error);
 
 		error = pdf_loadpage(&app->page, app->xref, obj);
 		if (error)
 			pdfapp_error(app, error);
 
 		sprintf(buf, "%s - %d/%d", app->doctitle,
-				app->pageno, pdf_getpagecount(app->pages));
+				app->pageno, app->xref->pagecount);
 		wintitle(app, buf);
 	}
 
@@ -321,24 +312,21 @@ static void pdfapp_gotouri(pdfapp_t *app, fz_obj *uri)
 
 static void pdfapp_gotopage(pdfapp_t *app, fz_obj *obj)
 {
-	int oid = fz_tonum(obj);
-	int i;
+	fz_error error;
+	int page;
 
-	for (i = 0; i < pdf_getpagecount(app->pages); i++)
+	error = pdf_findpageobject(app->xref, obj, &page);
+	if (error)
+		pdfapp_error(app, error);
+
+	if (app->histlen + 1 == 256)
 	{
-		if (fz_tonum(app->pages->pref[i]) == oid)
-		{
-			if (app->histlen + 1 == 256)
-			{
-				memmove(app->hist, app->hist + 1, sizeof(int) * 255);
-				app->histlen --;
-			}
-			app->hist[app->histlen++] = app->pageno;
-			app->pageno = i + 1;
-			pdfapp_showpage(app, 1, 1);
-			return;
-		}
+		memmove(app->hist, app->hist + 1, sizeof(int) * 255);
+		app->histlen --;
 	}
+	app->hist[app->histlen++] = app->pageno;
+	app->pageno = page;
+	pdfapp_showpage(app, 1, 1);
 }
 
 void pdfapp_onresize(pdfapp_t *app, int w, int h)
@@ -453,7 +441,7 @@ void pdfapp_onkey(pdfapp_t *app, int c)
 		break;
 
 	case 'G':
-		app->pageno = pdf_getpagecount(app->pages);
+		app->pageno = app->xref->pagecount;
 		break;
 
 	case 'm':
@@ -494,8 +482,8 @@ void pdfapp_onkey(pdfapp_t *app, int c)
 
 	if (app->pageno < 1)
 		app->pageno = 1;
-	if (app->pageno > pdf_getpagecount(app->pages))
-		app->pageno = pdf_getpagecount(app->pages);
+	if (app->pageno > app->xref->pagecount)
+		app->pageno = app->xref->pagecount;
 
 	if (app->pageno != oldpage)
 	{

mupdf/apps/common/pdftool.c
@@ -2,8 +2,7 @@
 
 char *basename = nil;
 pdf_xref *xref = nil;
-pdf_pagetree *pagetree = nil;
-void (*cleanup)(void) = nil;
+static void (*cleanup)(void) = nil;
 
 void closexref(void);
 
@@ -75,12 +74,6 @@ void closexref(void)
 	if (cleanup)
 		cleanup();
 
-	if (pagetree)
-	{
-		pdf_droppagetree(pagetree);
-		pagetree = nil;
-	}
-
 	if (xref)
 	{
 		pdf_closexref(xref);
@@ -88,14 +81,5 @@ void closexref(void)
 	}
 
 	basename = nil;
-}
-
-void loadpagetree(void)
-{
-	fz_error error;
-
-	error = pdf_loadpagetree(&pagetree, xref);
-	if (error)
-		die(error);
 }
 

mupdf/apps/mozilla/moz_main.c
@@ -16,7 +16,6 @@ typedef struct page_s page_t;
 
 struct page_s
 {
-    fz_obj *ref;
     fz_obj *obj;
     pdf_page *page;
     fz_pixmap *image;
@@ -118,20 +117,11 @@ void pdfmoz_open(pdfmoz_t *moz, char *filename)
 	    pdfmoz_warn(moz, "Invalid password.");
     }
 
-    /*
-     * Load page tree
-     */
-
-    error = pdf_loadpagetree(&pages, moz->xref);
-    if (error)
-	pdfmoz_error(moz, error);
-
-    moz->pagecount = pdf_getpagecount(pages);
+    moz->pagecount = moz->xrex->pagecount;
     moz->pages = fz_malloc(sizeof(page_t) * moz->pagecount);
 
     for (i = 0; i < moz->pagecount; i++)
     {
-	moz->pages[i].ref = fz_keepobj(pages->pref[i]);
 	moz->pages[i].obj = fz_keepobj(pdf_getpageobject(pages, i));
 	moz->pages[i].page = nil;
 	moz->pages[i].image = nil;
@@ -290,28 +280,32 @@ void pdfmoz_gotouri(pdfmoz_t *moz, fz_obj *uri)
 
 int pdfmoz_getpagenum(pdfmoz_t *moz, fz_obj *obj)
 {
-    int oid = fz_tonum(obj);
-    int i;
-    for (i = 0; i < moz->pagecount; i++)
-	if (fz_tonum(moz->pages[i].ref) == oid)
-	    return i;
-    return 0;
+    fz_error error;
+    int page;
+    int i, y = 0;
+
+    error = pdf_findpageobject(moz->xref, obj, &page);
+    if (error)
+	pdfmoz_error(moz, error);
+
+    return page;
 }
 
 void pdfmoz_gotopage(pdfmoz_t *moz, fz_obj *obj)
 {
-    int oid = fz_tonum(obj);
+    fz_error error;
+    int page;
     int i, y = 0;
-    for (i = 0; i < moz->pagecount; i++)
-    {
-	if (fz_tonum(moz->pages[i].ref) == oid)
-	{
-	    SetScrollPos(moz->hwnd, SB_VERT, y, TRUE);
-	    InvalidateRect(moz->hwnd, NULL, FALSE);
-	    return;
-	}
+
+    error = pdf_findpageobject(moz->xref, obj, &page);
+    if (error)
+	pdfmoz_error(moz, error);
+
+    for (i = 0; i < page; i++)
 	y += moz->pages[i].px;
-    }
+
+    SetScrollPos(moz->hwnd, SB_VERT, y, TRUE);
+    InvalidateRect(moz->hwnd, NULL, FALSE);
 }
 
 void pdfmoz_onmouse(pdfmoz_t *moz, int x, int y, int click)

mupdf/apps/pdfapp.h
@@ -26,8 +26,8 @@ struct pdfapp_s
 	char *doctitle;
 	pdf_xref *xref;
 	pdf_outline *outline;
-	pdf_pagetree *pages;
 	fz_renderer *rast;
+	int pagecount;
 
 	/* current view params */
 	float zoom;

mupdf/apps/pdfdraw.c
@@ -93,7 +93,9 @@ static void drawloadpage(int pagenum, struct benchmark *loadtimes)
 	gettime(&start);
     }
 
-    pageobj = pdf_getpageobject(pagetree, pagenum - 1);
+    error = pdf_getpageobject(xref, pagenum, &pageobj);
+    if (error)
+	die(error);
     error = pdf_loadpage(&drawpage, xref, pageobj);
     if (error)
 	die(error);
@@ -324,7 +326,7 @@ static void drawpages(char *pagelist)
 	    if (strlen(dash) > 1)
 		epage = atoi(dash + 1);
 	    else
-		epage = pdf_getpagecount(pagetree);
+		epage = xref->pagecount;
 	}
 
 	if (spage > epage)
@@ -332,8 +334,8 @@ static void drawpages(char *pagelist)
 
 	if (spage < 1)
 	    spage = 1;
-	if (epage > pdf_getpagecount(pagetree))
-	    epage = pdf_getpagecount(pagetree);
+	if (epage > xref->pagecount)
+	    epage = xref->pagecount;
 
 	printf("Drawing pages %d-%d...\n", spage, epage);
 	for (page = spage; page <= epage; page++)
@@ -412,7 +414,6 @@ int main(int argc, char **argv)
 		die(error);
 
 	    openxref(argv[fz_optind], password, 0);
-	    loadpagetree();
 	    state = NO_PAGES_DRAWN;
 	}
 	else

commit 0c1c72ef489e7e2d929fda7bfe87e78067eb216b

temporary fix for issue 572: Regression from r1293

mupdf/mupdf/pdf_open.c
@@ -712,7 +712,8 @@ pdf_loadxref2(pdf_xref *xref)
 		goto cleanup;
 	}
 
-	error = pdf_getpagecount(xref, &xref->pagecount);
+	// TODO: this fails for encrypted documents, so leave it to callers for now
+	// error = pdf_getpagecount(xref, &xref->pagecount);
 	if (error)
 	{
 		error = fz_rethrow(error, "cannot determine page count");

mupdf/mupdf/pdf_repair.c
@@ -346,7 +346,8 @@ pdf_repairxref2(pdf_xref *xref, fz_stream *file)
 		}
 	}
 
-	error = pdf_getpagecount(xref, &xref->pagecount);
+	// TODO: this fails for encrypted documents, so leave it to callers for now
+	// error = pdf_getpagecount(xref, &xref->pagecount);
 	if (error)
 		return fz_rethrow(error, "cannot determine page count");
 

src/PdfEngine.cc
@@ -242,6 +242,12 @@ DecryptedOk:
     if (_xref->info)
         fz_keepobj(_xref->info);
 
+    // TODO: MuPDF would like to call this from pdf_loadxref and pdf_repairxref,
+    //       but currently fails for encrypted documents
+    error = pdf_getpagecount(_xref, &_xref->pagecount);
+    if (error)
+        goto Error;
+
     error = pdf_loadnametrees(_xref);
     if (error)
         goto Error;