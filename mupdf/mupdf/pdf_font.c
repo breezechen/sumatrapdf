breezechen/sumatrapdf

commit 5331c60830535dbf069fcf5558ec7d7a6286e26c

add several MuPDF work-arounds to fix issues 115, 130, 344 and 424
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@1272 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/mupdf/pdf_font.c
@@ -343,6 +343,8 @@ loadsimplefont(pdf_fontdesc **fontdescp, pdf_xref *xref, fz_obj *dict, fz_obj *r
 				pdf_loadencoding(estrings, fz_toname(base));
 			else if (!fontdesc->isembedded)
 				pdf_loadencoding(estrings, "StandardEncoding");
+			else if (!fontdesc->encoding)
+				pdf_loadencoding(estrings, "StandardEncoding");
 
 			diff = fz_dictgets(encoding, "Differences");
 			if (fz_isarray(diff))
@@ -437,13 +439,37 @@ loadsimplefont(pdf_fontdesc **fontdescp, pdf_xref *xref, fz_obj *dict, fz_obj *r
 		{
 			etable[i] = ftcharindex(face, i);
 			fterr = FT_Get_Glyph_Name(face, etable[i], ebuffer[i], 32);
-			if (fterr)
+			// Without glyph names, we can still display the font, even if we don't know what the text says
+			if (fterr && FT_HAS_GLYPH_NAMES(face))
 			{
 				error = fz_throw("freetype get glyph name (gid %d): %s", etable[i], ft_errorstring(fterr));
 				goto cleanup;
 			}
 			if (ebuffer[i][0])
 				estrings[i] = ebuffer[i];
+		}
+
+		/* Load encoding Differences nonetheless, when they're available */
+		if (fz_isdict(encoding))
+		{
+			fz_obj *diff, *item;
+
+			diff = fz_dictgets(encoding, "Differences");
+			if (fz_isarray(diff))
+			{
+				n = fz_arraylen(diff);
+				k = 0;
+				for (i = 0; i < n; i++)
+				{
+					item = fz_arrayget(diff, i);
+					if (fz_isint(item))
+						k = fz_toint(item);
+					if (fz_isname(item))
+						estrings[k++] = fz_toname(item);
+					if (k < 0) k = 0;
+					if (k > 255) k = 255;
+				}
+			}
 		}
 	}
 

mupdf/fitz/filt_pipeline.c
@@ -129,7 +129,17 @@ tail:
 	}
 
 	else if (e == fz_iodone)
+	{
+		if (!p->head->done)
+		{
+			// TODO: Should this ever be allowed to happen?
+			// If tail was done but head wasn't, there must have been a (small?)
+			// trailer left in the queue - let the head read over it
+			e = fz_process(p->head, in, p->buffer);
+			assert(e == fz_iodone);
+		}
 		return fz_iodone;
+	}
 
 	else if (e)
 		return fz_rethrow(e, "cannot process tail filter");

mupdf/mupdf/pdf_interpret.c
@@ -799,6 +799,8 @@ Lsetcolor:
 				return fz_throw("cannot find Font dictionary");
 
 			obj = fz_dictget(dict, csi->stack[0]);
+			if (!obj && dict->u.d.len > 0)
+				obj = dict->u.d.items[0].v; // Just fall back to any font, so that we can go on
 			if (!obj)
 				return fz_throw("cannot find font resource: %s", fz_toname(csi->stack[0]));
 

src/DisplayModel.cc
@@ -1426,7 +1426,7 @@ bool DisplayModel::cvtUserToScreen(int pageNo, double *x, double *y)
     double zoom = zoomReal();
     int rot = rotation();
     fz_point p;
-    assert(page);
+    // assert(page);
     if(!page)
         return false;
 
@@ -1469,6 +1469,8 @@ bool DisplayModel::cvtScreenToUser(int *pageNo, double *x, double *y)
         return false;
 
     pdf_page *page = pdfEngine->getPdfPage(*pageNo);
+    if (!page)
+        return false;
 
     const PdfPageInfo *pageInfo = getPageInfo(*pageNo);
 

commit 29587207e157fd4992c2badd56c4137c0d8a5994

fix issue 595 (regression from r1272): New version only shows special characters.

mupdf/mupdf/pdf_font.c
@@ -343,7 +343,7 @@ loadsimplefont(pdf_fontdesc **fontdescp, pdf_xref *xref, fz_obj *dict, fz_obj *r
 				pdf_loadencoding(estrings, fz_toname(base));
 			else if (!fontdesc->isembedded)
 				pdf_loadencoding(estrings, "StandardEncoding");
-			else if (!fontdesc->encoding)
+			else if (!fontdesc->encoding && !symbolic)
 				pdf_loadencoding(estrings, "StandardEncoding");
 
 			diff = fz_dictgets(encoding, "Differences");