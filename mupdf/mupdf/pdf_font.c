breezechen/sumatrapdf

commit 30817154d2f639385f4e041ade4f64c6bee42ab8

fix part of issue 664: Some east-european characters are not displayed
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@1591 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/mupdf/pdf_font.c
@@ -310,7 +310,10 @@ loadsimplefont(pdf_fontdesc **fontdescp, pdf_xref *xref, fz_obj *dict)
 
 		if (kind == TYPE1)
 		{
-			if (test->platform_id == 7)
+			/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=664 */
+			if (test->platform_id == 3 && test->encoding_id == 1)
+				cmap = test;
+			if (test->platform_id == 7 && test->encoding_id == 2)
 				cmap = test;
 		}
 

commit 6388c656fcc63eb4a95b2b9be81369eb2d5c885e

fix issues 870 and 871: ligatures (fi, fl, etc.) not rendered/misrendered (regression from r1591)

mupdf/mupdf/pdf_font.c
@@ -366,10 +366,7 @@ loadsimplefont(pdf_fontdesc **fontdescp, pdf_xref *xref, fz_obj *dict)
 
 		if (kind == TYPE1)
 		{
-			/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=664 */
-			if (test->platform_id == 3 && test->encoding_id == 1)
-				cmap = test;
-			if (test->platform_id == 7 && test->encoding_id == 2)
+			if (test->platform_id == 7)
 				cmap = test;
 		}
 