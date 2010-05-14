d87e4afb690e10344eac5b1326477668254a5cb0
zeniko <zeniko@841c7e00-0c1d-0410-a694-8ba26473ccb1>
2010-05-14 12:51:33 +0000

    fix version cropping in About window

--- src/SumatraAbout.cpp
+++ src/SumatraAbout.cpp
@@ -12,9 +12,7 @@
 #define ABOUT_BOX_MARGIN_DY         6
 #define ABOUT_BORDER_COL            RGB(0,0,0)
 #define ABOUT_TXT_DY                6
-
 #define ABOUT_RECT_PADDING          8
-#define MIN_ABOUT_DX                326 // chosen so that (dbg) in debug builds is not cut off
 
 #define ABOUT_WIN_TITLE         _TR("About SumatraPDF")
 
@@ -24,16 +22,23 @@
 #define SUMATRA_TXT_FONT        _T("Arial Black")
 #define SUMATRA_TXT_FONT_SIZE   24
 
-#define BETA_TXT_FONT           _T("Arial Black")
-#define BETA_TXT_FONT_SIZE      12
+#define VERSION_TXT_FONT        _T("Arial Black")
+#define VERSION_TXT_FONT_SIZE   12
 
 #ifdef SVN_PRE_RELEASE_VER
- #define BETA_TXT                _T("Pre-Release")
+ #define VERSION_TXT            _T("v") _T(CURR_VERSION) _T(".") _T(QM(SVN_PRE_RELEASE_VER))
+ #define VERSION_SUB_TXT        _T("Pre-release")
 #else
  #ifdef DEBUG
- #define BETA_TXT                _T(" v") _T(CURR_VERSION) _T(" (dbg)")
+  #define VERSION_TXT           _T("v") _T(CURR_VERSION) _T(" (dbg)")
+  #define VERSION_SUB_TXT       _T("")
  #else
- #define BETA_TXT                _T(" v") _T(CURR_VERSION)
+  #define VERSION_TXT           _T("v") _T(CURR_VERSION)
+  #ifdef BUILD_RM_VERSION
+   #define VERSION_SUB_TXT      _T("Adapted by RM")
+  #else
+   #define VERSION_SUB_TXT      _T("")
+  #endif
  #endif
 #endif
 
@@ -61,6 +66,10 @@ static AboutLayoutInfoEl gAboutLayoutInfo[] = {
 #endif 
 #ifdef SVN_PRE_RELEASE_VER
     { _T("a note"), _T("Pre-release version, for testing only!"), NULL,
+    0, 0, 0, 0, 0, 0, 0, 0 },
+#endif
+#ifdef DEBUG
+    { _T("a note"), _T("Debug version, for testing only!"), NULL,
     0, 0, 0, 0, 0, 0, 0, 0 },
 #endif
     { _T("pdf rendering"), _T("MuPDF"), _T("http://mupdf.com"),
@@ -108,11 +117,11 @@ void DrawAbout(HWND hwnd, HDC hdc, RECT *rect)
     HPEN penLinkLine = CreatePen(PS_SOLID, ABOUT_LINE_SEP_SIZE, COL_BLUE_LINK);
 
     HFONT fontSumatraTxt = Win32_Font_GetSimple(hdc, SUMATRA_TXT_FONT, SUMATRA_TXT_FONT_SIZE);
-    HFONT fontBetaTxt = Win32_Font_GetSimple(hdc, BETA_TXT_FONT, BETA_TXT_FONT_SIZE);
+    HFONT fontVersionTxt = Win32_Font_GetSimple(hdc, VERSION_TXT_FONT, VERSION_TXT_FONT_SIZE);
     HFONT fontLeftTxt = Win32_Font_GetSimple(hdc, LEFT_TXT_FONT, LEFT_TXT_FONT_SIZE);
     HFONT fontRightTxt = Win32_Font_GetSimple(hdc, RIGHT_TXT_FONT, RIGHT_TXT_FONT_SIZE);
 
-    HFONT origFont = (HFONT)SelectObject(hdc, fontSumatraTxt); /* Just to remember the orig font */
+    HGDIOBJ origFont = SelectObject(hdc, fontSumatraTxt); /* Just to remember the orig font */
 
     SetBkMode(hdc, TRANSPARENT);
 
@@ -139,32 +148,20 @@ void DrawAbout(HWND hwnd, HDC hdc, RECT *rect)
     Rectangle(hdc, offX, offY + ABOUT_LINE_OUTER_SIZE, offX + totalDx, offY + boxDy + ABOUT_LINE_OUTER_SIZE);
 
     SetTextColor(hdc, ABOUT_BORDER_COL);
-    (HFONT)SelectObject(hdc, fontSumatraTxt);
+    SelectObject(hdc, fontSumatraTxt);
     x = offX + (totalDx - sumatraPdfTxtDx) / 2;
     y = offY + (boxDy - sumatraPdfTxtDy) / 2;
     txt = SUMATRA_TXT;
     TextOut(hdc, x, y, txt, lstrlen(txt));
 
-    (HFONT)SelectObject(hdc, fontBetaTxt);
+    SelectObject(hdc, fontVersionTxt);
     x = offX + (totalDx - sumatraPdfTxtDx) / 2 + sumatraPdfTxtDx + 6;
     y = offY + (boxDy - sumatraPdfTxtDy) / 2;
-    txt = BETA_TXT;
+    txt = VERSION_TXT;
     TextOut(hdc, x, y, txt, lstrlen(txt));
-
-#ifdef BUILD_RM_VERSION
-    txt = _T("Adapted by RM");
+    txt = VERSION_SUB_TXT;
     TextOut(hdc, x, y + 16, txt, lstrlen(txt));
-#endif
 
-#ifdef SVN_PRE_RELEASE_VER
-    GetTextExtentPoint32(hdc, txt, lstrlen(txt), &txtSize);
-    y += (int)txtSize.cy + 2;
-
-    TCHAR buf[128];
-    _sntprintf(buf, dimof(buf), _T("v%s svn %d"), _T(CURR_VERSION), SVN_PRE_RELEASE_VER);
-    txt = &(buf[0]);
-    TextOut(hdc, x, y, txt, lstrlen(txt));
-#endif
     SetTextColor(hdc, ABOUT_BORDER_COL);
 
     offY += boxDy;
@@ -172,7 +169,7 @@ void DrawAbout(HWND hwnd, HDC hdc, RECT *rect)
 
     /* render text on the left*/
     leftLargestDx = 0;
-    (HFONT)SelectObject(hdc, fontLeftTxt);
+    SelectObject(hdc, fontLeftTxt);
     for (int i = 0; gAboutLayoutInfo[i].leftTxt != NULL; i++) {
         txt = gAboutLayoutInfo[i].leftTxt;
         x = gAboutLayoutInfo[i].leftTxtPosX;
@@ -184,7 +181,7 @@ void DrawAbout(HWND hwnd, HDC hdc, RECT *rect)
     }
 
     /* render text on the right */
-    (HFONT)SelectObject(hdc, fontRightTxt);
+    SelectObject(hdc, fontRightTxt);
     for (int i = 0; gAboutLayoutInfo[i].leftTxt != NULL; i++) {
         bool hasUrl = !gRestrictedUse && gAboutLayoutInfo[i].url;
         SetTextColor(hdc, hasUrl ? COL_BLUE_LINK : ABOUT_BORDER_COL);
@@ -213,7 +210,7 @@ void DrawAbout(HWND hwnd, HDC hdc, RECT *rect)
 
     SelectObject(hdc, origFont);
     Win32_Font_Delete(fontSumatraTxt);
-    Win32_Font_Delete(fontBetaTxt);
+    Win32_Font_Delete(fontVersionTxt);
     Win32_Font_Delete(fontLeftTxt);
     Win32_Font_Delete(fontRightTxt);
 
@@ -228,24 +225,36 @@ void UpdateAboutLayoutInfo(HWND hwnd, HDC hdc, RECT * rect)
     SIZE            txtSize;
     int             totalDx, totalDy;
     int             leftDy, rightDy;
-    int             leftLargestDx, rightLargestDx;
+    int             leftLargestDx, rightLargestDx, titleLargestDx;
     int             linePosX, linePosY;
     int             currY;
     int             offX, offY;
     int             boxDy;
 
     HFONT fontSumatraTxt = Win32_Font_GetSimple(hdc, SUMATRA_TXT_FONT, SUMATRA_TXT_FONT_SIZE);
+    HFONT fontVersionTxt = Win32_Font_GetSimple(hdc, VERSION_TXT_FONT, VERSION_TXT_FONT_SIZE);
     HFONT fontLeftTxt = Win32_Font_GetSimple(hdc, LEFT_TXT_FONT, LEFT_TXT_FONT_SIZE);
     HFONT fontRightTxt = Win32_Font_GetSimple(hdc, RIGHT_TXT_FONT, RIGHT_TXT_FONT_SIZE);
-    HFONT origFont = (HFONT)SelectObject(hdc, fontSumatraTxt);
+    HGDIOBJ origFont = SelectObject(hdc, fontSumatraTxt);
 
-    /* calculate top box height */
+    /* calculate minimal top box size */
     const TCHAR *txt = SUMATRA_TXT;
     GetTextExtentPoint32(hdc, txt, lstrlen(txt), &txtSize);
     boxDy = txtSize.cy + ABOUT_BOX_MARGIN_DY * 2;
+    titleLargestDx = txtSize.cx;
+
+    /* consider version and version-sub strings */
+    SelectObject(hdc, fontVersionTxt);
+    txt = VERSION_TXT;
+    GetTextExtentPoint32(hdc, txt, lstrlen(txt), &txtSize);
+    offX = txtSize.cx;
+    txt = VERSION_SUB_TXT;
+    GetTextExtentPoint32(hdc, txt, lstrlen(txt), &txtSize);
+    txtSize.cx = max(txtSize.cx, offX);
+    titleLargestDx += 2 * (txtSize.cx + 6);
 
     /* calculate left text dimensions */
-    (HFONT)SelectObject(hdc, fontLeftTxt);
+    SelectObject(hdc, fontLeftTxt);
     leftLargestDx = 0;
     leftDy = 0;
     for (int i = 0; gAboutLayoutInfo[i].leftTxt != NULL; i++) {
@@ -263,7 +272,7 @@ void UpdateAboutLayoutInfo(HWND hwnd, HDC hdc, RECT * rect)
     }
 
     /* calculate right text dimensions */
-    (HFONT)SelectObject(hdc, fontRightTxt);
+    SelectObject(hdc, fontRightTxt);
     rightLargestDx = 0;
     rightDy = 0;
     for (int i = 0; gAboutLayoutInfo[i].leftTxt != NULL; i++) {
@@ -281,11 +290,11 @@ void UpdateAboutLayoutInfo(HWND hwnd, HDC hdc, RECT * rect)
     }
 
     /* calculate total dimension and position */
-    totalDx  = ABOUT_LINE_OUTER_SIZE + ABOUT_MARGIN_DX + leftLargestDx;
+    totalDx  = leftLargestDx + rightLargestDx;
     totalDx += ABOUT_LEFT_RIGHT_SPACE_DX + ABOUT_LINE_SEP_SIZE + ABOUT_LEFT_RIGHT_SPACE_DX;
-    totalDx += rightLargestDx + ABOUT_MARGIN_DX + ABOUT_LINE_OUTER_SIZE;
-    if (totalDx < MIN_ABOUT_DX)
-        totalDx = MIN_ABOUT_DX;
+    if (totalDx < titleLargestDx)
+        totalDx = titleLargestDx;
+    totalDx += 2 * ABOUT_LINE_OUTER_SIZE + 2 * ABOUT_MARGIN_DX;
 
     totalDy  = boxDy;
     totalDy += ABOUT_LINE_OUTER_SIZE;
@@ -319,6 +328,7 @@ void UpdateAboutLayoutInfo(HWND hwnd, HDC hdc, RECT * rect)
 
     SelectObject(hdc, origFont);
     Win32_Font_Delete(fontSumatraTxt);
+    Win32_Font_Delete(fontVersionTxt);
     Win32_Font_Delete(fontLeftTxt);
     Win32_Font_Delete(fontRightTxt);
 }
