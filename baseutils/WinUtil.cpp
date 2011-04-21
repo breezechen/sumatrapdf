breezechen/sumatrapdf

commit 02899d6aa9d3d60251d88b4f53a7d90fcd1c938f

always hide ToC box and selection when browsing in a folder (fixes issue 1379)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@3431 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/SumatraPDF.cpp
@@ -1470,6 +1470,7 @@ static bool LoadDocIntoWindow(
 
     float zoomVirtual = gGlobalPrefs.m_defaultZoom;
     int rotation = DEFAULT_ROTATION;
+    bool oldTocShow = win->tocShow;
 
     if (state) {
         if (win->dm->validPageNo(startPage)) {
@@ -1564,14 +1565,14 @@ Error:
         win->ClearTocBox();
     if (win->IsDocLoaded())
         win->dm->SetScrollState(ss);
-    if (win->IsDocLoaded() && win->tocShow) {
-        if (win->dm->pdfEngine && win->dm->pdfEngine->HasToCTree()) {
-            win->ShowTocBox();
-        } else {
-            // Hide the now useless ToC sidebar and force an update afterwards
-            win->HideTocBox();
-            win->RedrawAll(true);
-        }
+    if (win->IsDocLoaded() && win->tocShow &&
+        win->dm->pdfEngine && win->dm->pdfEngine->HasToCTree()) {
+        win->ShowTocBox();
+    } else if (oldTocShow) {
+        // Hide the now useless ToC sidebar and force an update afterwards
+        win->HideTocBox();
+        win->ClearTocBox();
+        win->RedrawAll(true);
     }
     UpdateToolbarAndScrollbarsForAllWindows();
     if (!win->IsDocLoaded()) {
@@ -3018,9 +3019,9 @@ static void CloseWindow(WindowInfo *win, bool quitIfLast, bool forceClose=false)
         }
         UpdateToolbarPageText(win, 0);
         UpdateToolbarFindText(win);
+        DeleteOldSelectionInfo(win, true);
         win->RedrawAll();
         WindowInfo_UpdateFindbox(win);
-        DeleteOldSelectionInfo(win, true);
     } else {
         HWND hwndToDestroy = win->hwndFrame;
         WindowInfo_Delete(win);
@@ -3732,6 +3733,7 @@ static void BrowseFolder(WindowInfo *win, bool forward)
 
     ScopedMem<TCHAR> fullpath(Path::Join(dir, files[index]));
     UpdateCurrentFileDisplayStateForWin(win);
+    DeleteOldSelectionInfo(win, true);
     LoadDocument(fullpath, win, true, true);
 }
 

commit 52a7fb3b3272aff71ea728ed94f7f1744eb63989

fix undefined variable assertion introduced with r3431

baseutils/WinUtil.cpp
@@ -485,6 +485,11 @@ DoubleBuffer::DoubleBuffer(HWND hwnd, RectI rect) :
     if (!hdcBuffer)
         return;
 
+    if (rect.x != 0 || rect.y != 0) {
+        SetGraphicsMode(hdcBuffer, GM_ADVANCED);
+        XFORM ctm = { 1.0, 0, 0, 1.0, (float)-rect.x, (float)-rect.y };
+        SetWorldTransform(hdcBuffer, &ctm);
+    }
     DeleteObject(SelectObject(hdcBuffer, doubleBuffer));
 }
 

src/SumatraPDF.cpp
@@ -1489,6 +1489,7 @@ static bool LoadDocIntoWindow(
     win.dm = DisplayModel::CreateFromFileName(&win, fileName, displayMode,
         startPage, win.GetViewPortSize());
     bool needrefresh = !win.dm;
+    bool oldTocShow = win.tocShow;
 
     if (!win.dm) {
         assert(!win.IsDocLoaded() && !win.IsAboutWindow());
@@ -1512,7 +1513,6 @@ static bool LoadDocIntoWindow(
 
     float zoomVirtual = gGlobalPrefs.m_defaultZoom;
     int rotation = DEFAULT_ROTATION;
-    bool oldTocShow = win.tocShow;
 
     if (state) {
         if (win.dm->validPageNo(startPage)) {