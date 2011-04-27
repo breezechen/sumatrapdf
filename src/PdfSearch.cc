9b85a1147035f8c334dcc5741db4908024e73ea4
kkowalczyk <kkowalczyk@841c7e00-0c1d-0410-a694-8ba26473ccb1>
2011-04-27 05:51:08 +0000

    stndardize tagging for release version

---
+++ src/PdfSearch.cc
@@ -0,0 +1,214 @@
+#include "PdfSearch.h"
+
+#define NONE    MAXLONG
+
+#include "wstr_util.h"
+#include <ctype.h>
+
+void PdfSearchEngine::SetText(wchar_t *text)
+{
+    this->Clear();
+    this->length = wcslen(text);
+    this->text = wstr_dup(text);
+}
+
+// Fitz
+PdfSearchFitz::PdfSearchFitz(PdfEngineFitz *engine) : PdfSearchEngine()
+{
+    this->engine = engine;
+    this->line = NULL;
+    this->current = NULL;
+    this->last = NONE;
+}
+
+PdfSearchFitz::~PdfSearchFitz()
+{
+}
+
+void PdfSearchFitz::Reset()
+{
+    if (line)
+        pdf_droptextline(line);
+    line = current = NULL;
+    last = NONE;
+}
+
+void PdfSearchFitz::ReverseLineList()
+{
+    if (!line)
+        return;
+
+    pdf_textline *prev = line, *curr = line->next;
+    while (curr) {
+        pdf_textline *next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    line->next = NULL;
+    line = prev;
+}
+
+void PdfSearchFitz::SetDirection(bool forward)
+{
+    if (forward == this->forward)
+        return;
+    this->forward = forward;
+
+    if (forward)
+        last = last + 2;
+    else
+        last = last - 2;
+
+    ReverseLineList();
+}
+
+#define CHR(x) (WCHAR)(x)
+
+bool inline PdfSearchFitz::MatchChars(int c1, int c2)
+{
+    if (c1 == c2)
+        return true;
+    if (sensitive)
+        return false;
+    if (CharUpperW((LPWSTR)MAKELONG(CHR(c1),0)) == CharUpperW((LPWSTR)MAKELONG(CHR(c2),0)))
+        return true;
+    return false;
+}
+
+bool inline PdfSearchFitz::MatchAtPosition(int n)
+{
+    WCHAR *p = (WCHAR *)text;
+    result.left = current->text[n].bbox.x0;
+    result.top = current->text[n].bbox.y0;
+    last = n;
+
+    while (n < current->len && *p) {
+        if (!MatchChars((int)*p, current->text[n].c))
+            break;
+        p++;
+        n++;
+    }
+
+    if (*p == 0) { // Found
+        result.right = current->text[n-1].bbox.x1;
+        result.bottom = current->text[n-1].bbox.y1;
+        if (forward)
+            last = last + 1;
+        else
+            last = last - 1;
+        return true;
+    }
+    
+    return false;
+}
+
+// TODO:
+// Apply Boyer-Moore algorithm here
+bool PdfSearchFitz::FindTextInPage(int page)
+{
+    if (!text)
+        return false;
+
+    WCHAR p = *(WCHAR *)text;
+    int start = last;
+
+    if (forward) {
+        if (NONE == start)
+            start = 0;
+        while (current) {
+            for (int i = start; i < current->len; i++) {
+                if (MatchChars((int)p, current->text[i].c)) {
+                    if (MatchAtPosition(i))
+                        goto Found;
+                }
+            }
+            current = current->next;
+            start = 0;
+        }
+    } else {
+        if (current && NONE == start)
+            start = current->len - length;
+        while (current) {
+            for (int i = start; i >= 0; i--) {
+                if (MatchChars((int)p, current->text[i].c)) {
+                    if (MatchAtPosition(i))
+                        goto Found;
+                }
+            }
+            current = current->next;
+            if (current)
+                start = current->len - length;
+        }
+    }
+    return false;
+
+Found:
+    if (page > 0)
+        result.page = page;
+    return true;
+}
+
+bool PdfSearchFitz::FindStartingAtPage(int pageNo)
+{
+    if (!text)
+        return false;
+
+    int pageEnd, step;
+    int total = engine->pageCount();
+
+    if (forward) {
+        pageEnd = total + 1;
+        step = 1;
+    } else {
+        pageEnd = 0;
+        step = -1;
+    }
+
+    while (pageNo != pageEnd) {
+        UpdateTracker(pageNo, total);
+        Reset();
+
+        pdf_page *page = engine->getPdfPage(pageNo);
+        if (!page)
+            goto NextPage;
+
+        pdf_textline *line;
+        if (pdf_loadtextfromtree(&line, page->tree, fz_identity())) // if error
+            goto NextPage;
+
+        this->line = line;
+        if (!forward)
+            ReverseLineList();
+
+        this->current = this->line;
+        if (FindTextInPage(pageNo))
+            return true;
+
+    NextPage:
+        pageNo += step;
+    }
+    
+    return false;
+}
+
+bool PdfSearchFitz::FindFirst(int page, wchar_t *text)
+{
+    SetText(text);
+
+    return FindStartingAtPage(page);
+}
+
+bool PdfSearchFitz::FindNext()
+{
+    if (FindTextInPage())
+        return true;
+
+    int newPage;
+    if (forward) {
+        newPage = result.page + 1;
+    } else {
+        newPage = result.page - 1;
+    }
+    return FindStartingAtPage(newPage);
+}
