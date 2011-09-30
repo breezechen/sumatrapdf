breezechen/sumatrapdf

commit 0af1e7ef16851e6d9b7db56204a24bbb68df3dc1

try ignoring broken arrays without failing to parse the surrounding dict (fixes issue 1643)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4461 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/pdf/pdf_parse.c
@@ -361,7 +361,17 @@ skip:
 			error = pdf_parse_array(&val, xref, file, buf, cap);
 			if (error)
 			{
+				/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=1643 */
+				fz_catch(error, "ignoring broken array for '%s'", fz_to_name(key));
 				fz_drop_obj(key);
+				do
+				{
+					error = pdf_lex(&tok, file, buf, cap, &len);
+					if (!error && tok == PDF_TOK_CLOSE_DICT)
+						goto skip;
+				} while (!error && tok != PDF_TOK_CLOSE_ARRAY && tok != PDF_TOK_EOF && tok != PDF_TOK_OPEN_ARRAY && tok != PDF_TOK_OPEN_DICT);
+				if (!error && tok == PDF_TOK_CLOSE_ARRAY)
+					continue;
 				fz_drop_obj(dict);
 				return fz_rethrow(error, "cannot parse dict");
 			}

commit 366a75c2f9b9e7add42222558d2078dd52f0d207

fix potential crash introduced with r4461

mupdf/pdf/pdf_parse.c
@@ -372,6 +372,7 @@ skip:
 				} while (!error && tok != PDF_TOK_CLOSE_ARRAY && tok != PDF_TOK_EOF && tok != PDF_TOK_OPEN_ARRAY && tok != PDF_TOK_OPEN_DICT);
 				if (!error && tok == PDF_TOK_CLOSE_ARRAY)
 					continue;
+				error = fz_throw("cannot make sense of broken array after all");
 				fz_drop_obj(dict);
 				return fz_rethrow(error, "cannot parse dict");
 			}