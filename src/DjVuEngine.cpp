breezechen/sumatrapdf

commit adfed9ba84fdaeb03dbb85cb3ce3e03decfaf8e4

don't show page size in chm properties window; remove obsolete code; turn on the remaining chm-related string for translation; rename ToC => Toc for easier typing
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4553 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/Favorites.cpp
@@ -350,12 +350,6 @@ void RebuildFavMenu(WindowInfo *win, HMENU menu)
     }
 }
 
-void RebuildFavMenu(ChmWindowInfo *win, HMENU menu)
-{
-    // TODO: pass loadedFilePath and pageNo correctly
-    RebuildFavMenu(NULL, 0, menu);
-}
-
 void ToggleFavorites(WindowInfo *win)
 {
     if (gGlobalPrefs.favVisible) {
@@ -585,7 +579,7 @@ void AddFavorite(WindowInfo *win)
     ScopedMem<TCHAR> name;
     if (win->dm->HasTocTree()) {
         // use the current ToC heading as default name
-        DocTocItem *root = win->dm->engine->GetToCTree();
+        DocTocItem *root = win->dm->engine->GetTocTree();
         DocTocItem *item = ToCItemForPageNo(root, pageNo);
         if (item)
             name.Set(str::Dup(item->title));

src/PdfEngine.cpp
@@ -723,10 +723,10 @@ public:
     virtual PageElement *GetElementAtPos(int pageNo, PointD pt);
 
     virtual PageDestination *GetNamedDest(const TCHAR *name);
-    virtual bool HasToCTree() const {
+    virtual bool HasTocTree() const {
         return outline != NULL || attachments != NULL;
     }
-    virtual DocTocItem *GetToCTree();
+    virtual DocTocItem *GetTocTree();
 
     virtual bool HasPageLabels() { return _pagelabels != NULL; }
     virtual TCHAR *GetPageLabel(int pageNo);
@@ -1119,7 +1119,7 @@ PdfToCItem *CPdfEngine::BuildToCTree(pdf_outline *entry, int& idCounter)
     return node;
 }
 
-DocTocItem *CPdfEngine::GetToCTree()
+DocTocItem *CPdfEngine::GetTocTree()
 {
     PdfToCItem *node = NULL;
     int idCounter = 0;
@@ -2020,8 +2020,8 @@ public:
     virtual PageElement *GetElementAtPos(int pageNo, PointD pt);
 
     virtual PageDestination *GetNamedDest(const TCHAR *name);
-    virtual bool HasToCTree() const { return _outline != NULL; }
-    virtual DocTocItem *GetToCTree();
+    virtual bool HasTocTree() const { return _outline != NULL; }
+    virtual DocTocItem *GetTocTree();
 
     int FindPageNo(const char *target);
     fz_rect FindDestRect(const char *target);
@@ -2789,9 +2789,9 @@ XpsToCItem *CXpsEngine::BuildToCTree(xps_outline *entry, int& idCounter)
     return node;
 }
 
-DocTocItem *CXpsEngine::GetToCTree()
+DocTocItem *CXpsEngine::GetTocTree()
 {
-    if (!HasToCTree())
+    if (!HasTocTree())
         return NULL;
 
     int idCounter = 0;

src/TableOfContents.cpp
@@ -331,7 +331,7 @@ void LoadTocTree(WindowInfo *win)
 
     win->tocRoot = NULL;
     if (win->dm->engine)
-        win->tocRoot = win->dm->engine->GetToCTree();
+        win->tocRoot = win->dm->engine->GetTocTree();
     if (!win->tocRoot)
         return;
 

commit 673879527e6f4a4679cdd4fb530f7bbbe078032d

fix DjVu ToC functions (regression from r4553)

src/DjVuEngine.cpp
@@ -126,10 +126,8 @@ class DjVuTocItem : public DocTocItem {
     DjVuDestination *dest;
 
 public:
-    bool isGeneric;
-
     DjVuTocItem(const char *title, const char *link) :
-        DocTocItem(str::conv::FromUtf8(title)), isGeneric(false) {
+        DocTocItem(str::conv::FromUtf8(title)) {
         dest = new DjVuDestination(link);
         pageNo = dest->GetDestPageNo();
     }
@@ -230,8 +228,8 @@ public:
     virtual PageElement *GetElementAtPos(int pageNo, PointD pt);
 
     virtual PageDestination *GetNamedDest(const TCHAR *name);
-    virtual bool HasToCTree() const { return outline != miniexp_nil; }
-    virtual DocTocItem *GetToCTree();
+    virtual bool HasTocTree() const { return outline != miniexp_nil; }
+    virtual DocTocItem *GetTocTree();
 
 protected:
     const TCHAR *fileName;
@@ -248,7 +246,7 @@ protected:
     bool ExtractPageText(miniexp_t item, const TCHAR *lineSep,
                          str::Str<TCHAR>& extracted, Vec<RectI>& coords);
     char *ResolveNamedDest(const char *name);
-    DjVuTocItem *BuildToCTree(miniexp_t entry, int& idCounter);
+    DjVuTocItem *BuildTocTree(miniexp_t entry, int& idCounter, int depth);
     bool Load(const TCHAR *fileName);
 };
 
@@ -719,7 +717,7 @@ PageDestination *CDjVuEngine::GetNamedDest(const TCHAR *name)
     return NULL;
 }
 
-DjVuTocItem *CDjVuEngine::BuildToCTree(miniexp_t entry, int& idCounter)
+DjVuTocItem *CDjVuEngine::BuildTocTree(miniexp_t entry, int& idCounter, int depth)
 {
     DjVuTocItem *node = NULL;
 
@@ -739,21 +737,14 @@ DjVuTocItem *CDjVuEngine::BuildToCTree(miniexp_t entry, int& idCounter)
         else if (!str::IsEmpty(name) && !str::Eq(name, link + 1))
             tocItem = new DjVuTocItem(name, linkNo);
         else {
-            ScopedMem<char> name(str::Format("Page %d", atoi(linkNo + 1)));
-            tocItem = new DjVuTocItem(name, linkNo);
-            // don't display such generic items by default
-            tocItem->isGeneric = true;
+            // ignore generic (name-less) entries
+            delete BuildTocTree(miniexp_cddr(item), idCounter, depth + 1);
+            continue;
         }
 
         tocItem->id = ++idCounter;
-        tocItem->child = BuildToCTree(miniexp_cddr(item), idCounter);
-        if (tocItem->child) {
-            // close items that only contain generic page references
-            tocItem->open = false;
-            for (DocTocItem *n = tocItem->child; n && !tocItem->open; n = n->next)
-                if (!static_cast<DjVuTocItem *>(n)->isGeneric)
-                    tocItem->open = true;
-        }
+        tocItem->child = BuildTocTree(miniexp_cddr(item), idCounter, depth + 1);
+        tocItem->open = depth <= 2;
 
         if (!node)
             node = tocItem;
@@ -764,14 +755,14 @@ DjVuTocItem *CDjVuEngine::BuildToCTree(miniexp_t entry, int& idCounter)
     return node;
 }
 
-DocTocItem *CDjVuEngine::GetToCTree()
+DocTocItem *CDjVuEngine::GetTocTree()
 {
-    if (!HasToCTree())
+    if (!HasTocTree())
         return NULL;
 
     ScopedCritSec scope(&gDjVuContext.lock);
     int idCounter = 0;
-    return BuildToCTree(outline, idCounter);
+    return BuildTocTree(outline, idCounter, 1);
 }
 
 bool DjVuEngine::IsSupportedFile(const TCHAR *fileName, bool sniff)

src/Favorites.cpp
@@ -554,7 +554,7 @@ void UpdateFavoritesTreeForAllWindows()
     }
 }
 
-static DocTocItem *ToCItemForPageNo(DocTocItem *item, int pageNo)
+static DocTocItem *TocItemForPageNo(DocTocItem *item, int pageNo)
 {
     DocTocItem *currItem = NULL;
 
@@ -565,7 +565,7 @@ static DocTocItem *ToCItemForPageNo(DocTocItem *item, int pageNo)
             break;
 
         // find any child item closer to the specified page
-        DocTocItem *subItem = ToCItemForPageNo(item->child, pageNo);
+        DocTocItem *subItem = TocItemForPageNo(item->child, pageNo);
         if (subItem)
             currItem = subItem;
     }
@@ -580,7 +580,7 @@ void AddFavorite(WindowInfo *win)
     if (win->dm->HasTocTree()) {
         // use the current ToC heading as default name
         DocTocItem *root = win->dm->engine->GetTocTree();
-        DocTocItem *item = ToCItemForPageNo(root, pageNo);
+        DocTocItem *item = TocItemForPageNo(root, pageNo);
         if (item)
             name.Set(str::Dup(item->title));
         delete root;

src/PdfEngine.cpp
@@ -668,7 +668,7 @@ struct PdfPageRun {
     int refs;
 };
 
-class PdfToCItem;
+class PdfTocItem;
 class PdfLink;
 
 class CPdfEngine : public PdfEngine {
@@ -769,7 +769,7 @@ protected:
                             fz_bbox clipbox=fz_infinite_bbox, bool cacheRun=true);
     void            DropPageRun(PdfPageRun *run, bool forceRemove=false);
 
-    PdfToCItem   *  BuildToCTree(pdf_outline *entry, int& idCounter);
+    PdfTocItem   *  BuildTocTree(pdf_outline *entry, int& idCounter);
     void            LinkifyPageText(pdf_page *page);
 
     int             FindPageNo(fz_obj *dest);
@@ -830,11 +830,11 @@ public:
     virtual TCHAR *GetValue() const { return str::Dup(content); }
 };
 
-class PdfToCItem : public DocTocItem {
+class PdfTocItem : public DocTocItem {
     PdfLink link;
 
 public:
-    PdfToCItem(TCHAR *title, PdfLink link) : DocTocItem(title), link(link) { }
+    PdfTocItem(TCHAR *title, PdfLink link) : DocTocItem(title), link(link) { }
 
     virtual PageDestination *GetLink() { return &link; }
 };
@@ -1096,20 +1096,20 @@ bool CPdfEngine::FinishLoading()
     return true;
 }
 
-PdfToCItem *CPdfEngine::BuildToCTree(pdf_outline *entry, int& idCounter)
+PdfTocItem *CPdfEngine::BuildTocTree(pdf_outline *entry, int& idCounter)
 {
-    PdfToCItem *node = NULL;
+    PdfTocItem *node = NULL;
 
     for (; entry; entry = entry->next) {
         TCHAR *name = entry->title ? str::conv::FromUtf8(entry->title) : str::Dup(_T(""));
-        PdfToCItem *item = new PdfToCItem(name, PdfLink(this, entry->link));
+        PdfTocItem *item = new PdfTocItem(name, PdfLink(this, entry->link));
         item->open = entry->count >= 0;
         item->id = ++idCounter;
 
         if (entry->link && PDF_LINK_GOTO == entry->link->kind)
             item->pageNo = FindPageNo(entry->link->dest);
         if (entry->child)
-            item->child = BuildToCTree(entry->child, idCounter);
+            item->child = BuildTocTree(entry->child, idCounter);
 
         if (!node)
             node = item;
@@ -1122,15 +1122,15 @@ PdfToCItem *CPdfEngine::BuildToCTree(pdf_outline *entry, int& idCounter)
 
 DocTocItem *CPdfEngine::GetTocTree()
 {
-    PdfToCItem *node = NULL;
+    PdfTocItem *node = NULL;
     int idCounter = 0;
 
     if (outline) {
-        node = BuildToCTree(outline, idCounter);
+        node = BuildTocTree(outline, idCounter);
         if (attachments)
-            node->AddSibling(BuildToCTree(attachments, idCounter));
+            node->AddSibling(BuildTocTree(attachments, idCounter));
     } else if (attachments)
-        node = BuildToCTree(attachments, idCounter);
+        node = BuildTocTree(attachments, idCounter);
 
     return node;
 }
@@ -1999,7 +1999,7 @@ struct XpsPageRun {
     int refs;
 };
 
-class XpsToCItem;
+class XpsTocItem;
 
 class CXpsEngine : public XpsEngine {
     friend XpsEngine;
@@ -2085,7 +2085,7 @@ protected:
                             fz_bbox clipbox=fz_infinite_bbox, bool cacheRun=true);
     void            dropPageRun(XpsPageRun *run, bool forceRemove=false);
 
-    XpsToCItem   *  BuildToCTree(xps_outline *entry, int& idCounter);
+    XpsTocItem   *  BuildTocTree(xps_outline *entry, int& idCounter, int depth);
     void            linkifyPageText(xps_page *page, int pageNo);
 
     RectD         * _mediaboxes;
@@ -2143,11 +2143,11 @@ public:
     virtual TCHAR *GetDestValue() const { return GetValue(); }
 };
 
-class XpsToCItem : public DocTocItem {
+class XpsTocItem : public DocTocItem {
     XpsLink link;
 
 public:
-    XpsToCItem(TCHAR *title, XpsLink link) : DocTocItem(title), link(link) { }
+    XpsTocItem(TCHAR *title, XpsLink link) : DocTocItem(title), link(link) { }
 
     virtual PageDestination *GetLink() { return &link; }
 };
@@ -2788,20 +2788,20 @@ PageDestination *CXpsEngine::GetNamedDest(const TCHAR *name)
     return NULL;
 }
 
-XpsToCItem *CXpsEngine::BuildToCTree(xps_outline *entry, int& idCounter)
+XpsTocItem *CXpsEngine::BuildTocTree(xps_outline *entry, int& idCounter, int depth)
 {
-    XpsToCItem *node = NULL;
+    XpsTocItem *node = NULL;
 
     for (; entry; entry = entry->next) {
         TCHAR *name = entry->title ? str::conv::FromUtf8(entry->title) : str::Dup(_T(""));
-        XpsToCItem *item = new XpsToCItem(name, XpsLink(this, entry->target, fz_empty_rect));
-        item->open = false;
+        XpsTocItem *item = new XpsTocItem(name, XpsLink(this, entry->target, fz_empty_rect));
         item->id = ++idCounter;
+        item->open = depth <= 2;
 
         if (str::Eq(item->GetLink()->GetType(), "ScrollTo"))
             item->pageNo = FindPageNo(entry->target);
         if (entry->child)
-            item->child = BuildToCTree(entry->child, idCounter);
+            item->child = BuildTocTree(entry->child, idCounter, depth + 1);
 
         if (!node)
             node = item;
@@ -2818,7 +2818,7 @@ DocTocItem *CXpsEngine::GetTocTree()
         return NULL;
 
     int idCounter = 0;
-    return BuildToCTree(_outline, idCounter);
+    return BuildTocTree(_outline, idCounter, 1);
 }
 
 bool XpsEngine::IsSupportedFile(const TCHAR *fileName, bool sniff)

src/TableOfContents.cpp
@@ -28,7 +28,7 @@ static void TreeView_ExpandRecursively(HWND hTree, HTREEITEM hItem, UINT flag, b
     }
 }
 
-static void CustomizeToCInfoTip(LPNMTVGETINFOTIP nmit)
+static void CustomizeTocInfoTip(LPNMTVGETINFOTIP nmit)
 {
     DocTocItem *tocItem = (DocTocItem *)nmit->lParam;
     ScopedMem<TCHAR> path(tocItem->GetLink() ? tocItem->GetLink()->GetDestValue() : NULL);
@@ -404,7 +404,7 @@ static LRESULT OnTocTreeNotify(WindowInfo *win, LPNMTREEVIEW pnmtv)
 #endif
 
         case TVN_GETINFOTIP:
-            CustomizeToCInfoTip((LPNMTVGETINFOTIP)pnmtv);
+            CustomizeTocInfoTip((LPNMTVGETINFOTIP)pnmtv);
             break;
     }
     return -1;
@@ -460,7 +460,7 @@ static LRESULT CALLBACK WndProcTocTree(HWND hwnd, UINT message, WPARAM wParam, L
     return CallWindowProc(DefWndProcTocTree, hwnd, message, wParam, lParam);
 }
 
-static void CustomizeToCInfoTip(LPNMTVGETINFOTIP nmit);
+static void CustomizeTocInfoTip(LPNMTVGETINFOTIP nmit);
 #ifdef DISPLAY_TOC_PAGE_NUMBERS
 static void RelayoutTocItem(LPNMTVCUSTOMDRAW ntvcd);
 #endif