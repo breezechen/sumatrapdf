breezechen/sumatrapdf

commit 29c0b90f09c923de04c2bde885b9cee0e93e0c46

merge latest MuPDF update
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4662 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/pdf/pdf_outline.c
@@ -1,13 +1,14 @@
 #include "fitz.h"
 #include "mupdf.h"
 
-static pdf_outline *
+static fz_outline *
 pdf_load_outline_imp(pdf_xref *xref, fz_obj *dict)
 {
-	pdf_outline *node;
+	pdf_link *link;
+	fz_outline *node;
 	fz_obj *obj;
 	/* SumatraPDF: prevent potential stack overflow */
-	pdf_outline *prev, *root = NULL;
+	fz_outline *prev, *root = NULL;
 	fz_obj *origDict = dict;
 
 	if (fz_is_null(dict))
@@ -22,27 +23,34 @@ pdf_load_outline_imp(pdf_xref *xref, fz_obj *dict)
 		fz_dict_puts(dict, ".seen", obj);
 		fz_drop_obj(obj);
 
-	node = fz_malloc(sizeof(pdf_outline));
+	node = fz_malloc(sizeof(fz_outline));
 	node->title = NULL;
-	node->link = NULL;
-	node->child = NULL;
+	node->page = -1;
+	node->down = NULL;
 	node->next = NULL;
-	node->count = 0;
 
 	obj = fz_dict_gets(dict, "Title");
 	if (obj)
 		node->title = pdf_to_utf8(obj);
 
-	obj = fz_dict_gets(dict, "Count");
-	if (obj)
-		node->count = fz_to_int(obj);
+	/* SumatraPDF: support expansion states */
+	node->is_open = fz_to_int(obj) >= 0;
+	/* SumatraPDF: extended outline actions */
+	node->data = node->free_data = NULL;
 
 	if (fz_dict_gets(dict, "Dest") || fz_dict_gets(dict, "A"))
-		node->link = pdf_load_link(xref, dict);
+	{
+		link = pdf_load_link(xref, dict);
+		if (link->kind == PDF_LINK_GOTO)
+			node->page = pdf_find_page_number(xref, fz_array_get(link->dest, 0));
+		/* SumatraPDF: extended outline actions */
+		node->data = link;
+		node->free_data = pdf_free_link;
+	}
 
 	obj = fz_dict_gets(dict, "First");
 	if (obj)
-		node->child = pdf_load_outline_imp(xref, obj);
+		node->down = pdf_load_outline_imp(xref, obj);
 
 		/* SumatraPDF: prevent potential stack overflow */
 		if (!root)
@@ -61,7 +69,7 @@ pdf_load_outline_imp(pdf_xref *xref, fz_obj *dict)
 	return node;
 }
 
-pdf_outline *
+fz_outline *
 pdf_load_outline(pdf_xref *xref)
 {
 	fz_obj *root, *obj, *first;
@@ -73,43 +81,4 @@ pdf_load_outline(pdf_xref *xref)
 		return pdf_load_outline_imp(xref, first);
 
 	return NULL;
-}
-
-void
-pdf_free_outline(pdf_outline *outline)
-{
-	if (outline->child)
-		pdf_free_outline(outline->child);
-	if (outline->next)
-		pdf_free_outline(outline->next);
-	if (outline->link)
-		pdf_free_link(outline->link);
-	fz_free(outline->title);
-	fz_free(outline);
-}
-
-void
-pdf_debug_outline(pdf_outline *outline, int level)
-{
-	int i;
-	while (outline)
-	{
-		for (i = 0; i < level; i++)
-			putchar(' ');
-
-		if (outline->title)
-			printf("%s ", outline->title);
-		else
-			printf("<NULL> ");
-
-		if (outline->link)
-			fz_debug_obj(outline->link->dest);
-		else
-			printf("<NULL>\n");
-
-		if (outline->child)
-			pdf_debug_outline(outline->child, level + 2);
-
-		outline = outline->next;
-	}
 }

mupdf/Makefile
@@ -2,7 +2,7 @@
 
 build ?= debug
 
-OUT := build/$(build)
+OUT ?= build/$(build)
 GEN := generated
 
 # --- Variables, Commands, etc... ---
@@ -41,6 +41,7 @@ $(OUT) $(GEN) :
 
 $(OUT)/%.a :
 	$(AR_CMD)
+	$(RANLIB_CMD)
 
 $(OUT)/% : $(OUT)/%.o
 	$(LINK_CMD)
@@ -75,7 +76,6 @@ $(MUPDF_LIB) : $(addprefix $(OUT)/, $(MUPDF_SRC:%.c=%.o))
 $(MUXPS_LIB) : $(addprefix $(OUT)/, $(MUXPS_SRC:%.c=%.o))
 
 libs: $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(THIRD_LIBS)
-	@ echo MuPDF/XPS and underlying libraries built
 
 # --- Generated CMAP and FONT files ---
 

mupdf/Makerules
@@ -34,6 +34,7 @@ ifeq "$(OS)" "Darwin"
 SYS_FREETYPE_INC := -I/usr/X11R6/include/freetype2
 CFLAGS += -I/usr/X11R6/include
 LDFLAGS += -L/usr/X11R6/lib
+RANLIB_CMD = ranlib $@
 X11_LIBS := -lX11 -lXext
 ifeq "$(arch)" "amd64"
 CFLAGS += -m64
@@ -56,6 +57,7 @@ endif
 #   cygwin)
 #   2) do a non cross compile build (e.g. windows in MSVC) first.
 #   3) download the generated files from mupdf.com.
+
 ifeq "$(OS)" "beagle-cross"
 CC = arm-none-linux-gnueabi-gcc
 LD = arm-none-linux-gnueabi-gcc
@@ -79,6 +81,18 @@ CC = arm-none-linux-gnueabi-gcc
 LD = arm-none-linux-gnueabi-gcc
 AR = arm-none-linux-gnueabi-ar
 CFLAGS += -O3 -mcpu=arm1136jf-s -mfpu=vfp -mfloat-abi=softfp -ffast-math -fsingle-precision-constant
+CROSSCOMPILE=yes
+NOX11=yes
+endif
+
+# Most variables when building for iOS are set up in ios/build_libs.sh,
+# which is called from the Xcode project as a "Run Script" build step.
+# The following section works for both device and simulator builds.
+
+ifeq "$(OS)" "ios"
+CC = $(PLATFORM_DEVELOPER_BIN_DIR)/clang
+AR = $(PLATFORM_DEVELOPER_BIN_DIR)/ar
+RANLIB_CMD = $(PLATFORM_DEVELOPER_BIN_DIR)/ranlib $@
 CROSSCOMPILE=yes
 NOX11=yes
 endif

mupdf/apps/pdfapp.c
@@ -139,7 +139,7 @@ static void pdfapp_open_pdf(pdfapp_t *app, char *filename, int fd)
 
 	app->outline = pdf_load_outline(app->xref);
 
-	app->doctitle = filename;
+	app->doctitle = fz_strdup(filename);
 	if (strrchr(app->doctitle, '\\'))
 		app->doctitle = strrchr(app->doctitle, '\\') + 1;
 	if (strrchr(app->doctitle, '/'))
@@ -174,7 +174,7 @@ static void pdfapp_open_xps(pdfapp_t *app, char *filename, int fd)
 		pdfapp_error(app, fz_rethrow(error, "cannot open document '%s'", filename));
 	fz_close(file);
 
-	app->doctitle = filename;
+	app->doctitle = fz_strdup(filename);
 
 	app->pagecount = xps_count_pages(app->xps);
 }
@@ -235,7 +235,7 @@ void pdfapp_close(pdfapp_t *app)
 	app->image = NULL;
 
 	if (app->outline)
-		pdf_free_outline(app->outline);
+		fz_free_outline(app->outline);
 	app->outline = NULL;
 
 	if (app->xref)

mupdf/apps/pdfapp.h
@@ -31,7 +31,7 @@ struct pdfapp_s
 	/* current document params */
 	char *doctitle;
 	pdf_xref *xref;
-	pdf_outline *outline;
+	fz_outline *outline;
 	xps_context *xps;
 
 	int pagecount;

mupdf/apps/pdfdraw.c
@@ -412,54 +412,14 @@ static void drawrange(pdf_xref *xref, char *range)
 	}
 }
 
-static int get_page_number(pdf_xref *xref, pdf_link *link)
-{
-	if (link->kind == PDF_LINK_GOTO)
-		return pdf_find_page_number(xref, fz_array_get(link->dest, 0));
-	return 0;
-}
-
-static void print_outline_xml(pdf_xref *xref, pdf_outline *outline, int level)
-{
-	int page;
-	printf("<outline>\n");
-	while (outline)
-	{
-		page = get_page_number(xref, outline->link);
-		printf("<link title=\"%s\" page=\"%d\" />\n",
-				outline->title ? outline->title : "<null>", page);
-		if (outline->child)
-			print_outline_xml(xref, outline->child, level + 1);
-		outline = outline->next;
-	}
-	printf("</outline>\n");
-}
-
-static void print_outline_plain(pdf_xref *xref, pdf_outline *outline, int level)
-{
-	int i, page;
-	while (outline)
-	{
-		page = get_page_number(xref, outline->link);
-		for (i = 0; i < level; i++)
-			putchar('\t');
-		printf("%s %d\n", outline->title ? outline->title : "<null>", page);
-		if (outline->child)
-			print_outline_plain(xref, outline->child, level + 1);
-		outline = outline->next;
-	}
-}
-
 static void drawoutline(pdf_xref *xref)
 {
-	pdf_outline *outline = pdf_load_outline(xref);
-	if (showoutline > 2)
-		pdf_debug_outline(outline, 0);
-	else if (showoutline > 1)
-		print_outline_xml(xref, outline, 0);
+	fz_outline *outline = pdf_load_outline(xref);
+	if (showoutline > 1)
+		fz_debug_outline_xml(outline, 0);
 	else
-		print_outline_plain(xref, outline, 0);
-	pdf_free_outline(outline);
+		fz_debug_outline(outline, 0);
+	fz_free_outline(outline);
 }
 
 int main(int argc, char **argv)

mupdf/apps/xpsdraw.c
@@ -16,6 +16,7 @@ int showxml = 0;
 int showtext = 0;
 int showtime = 0;
 int showmd5 = 0;
+int showoutline = 0;
 int savealpha = 0;
 int uselist = 1;
 
@@ -53,6 +54,7 @@ static void usage(void)
 		"\t-x\tshow display list\n"
 		"\t-d\tdisable use of display list\n"
 		"\t-5\tshow md5 checksums\n"
+		"\t-l\tprint outline\n"
 		"\tpages\tcomma separated list of ranges\n");
 	exit(1);
 }
@@ -391,6 +393,16 @@ static void drawrange(xps_context *ctx, char *range)
 	}
 }
 
+static void drawoutline(xps_context *ctx)
+{
+	fz_outline *outline = xps_load_outline(ctx);
+	if (showoutline > 1)
+		fz_debug_outline_xml(outline, 0);
+	else
+		fz_debug_outline(outline, 0);
+	fz_free_outline(outline);
+}
+
 int main(int argc, char **argv)
 {
 	int grayscale = 0;
@@ -399,7 +411,7 @@ int main(int argc, char **argv)
 	int code;
 	int c;
 
-	while ((c = fz_getopt(argc, argv, "o:p:r:Aadgmtx5")) != -1)
+	while ((c = fz_getopt(argc, argv, "o:p:r:Aadglmtx5")) != -1)
 	{
 		switch (c)
 		{
@@ -407,6 +419,7 @@ int main(int argc, char **argv)
 		case 'r': resolution = atof(fz_optarg); break;
 		case 'A': accelerate = 0; break;
 		case 'a': savealpha = 1; break;
+		case 'l': showoutline++; break;
 		case 'm': showtime++; break;
 		case 't': showtext++; break;
 		case 'x': showxml++; break;
@@ -420,7 +433,7 @@ int main(int argc, char **argv)
 	if (fz_optind == argc)
 		usage();
 
-	if (!showtext && !showxml && !showtime && !showmd5 && !output)
+	if (!showtext && !showxml && !showtime && !showmd5 && !showoutline && !output)
 	{
 		printf("nothing to do\n");
 		exit(0);
@@ -460,10 +473,16 @@ int main(int argc, char **argv)
 		if (showxml)
 			printf("<document name=\"%s\">\n", filename);
 
-		if (fz_optind == argc || !isrange(argv[fz_optind]))
-			drawrange(ctx, "1-");
-		if (fz_optind < argc && isrange(argv[fz_optind]))
-			drawrange(ctx, argv[fz_optind++]);
+		if (showoutline)
+			drawoutline(ctx);
+
+		if (showtext || showxml || showtime || showmd5 || output)
+		{
+			if (fz_optind == argc || !isrange(argv[fz_optind]))
+				drawrange(ctx, "1-");
+			if (fz_optind < argc && isrange(argv[fz_optind]))
+				drawrange(ctx, argv[fz_optind++]);
+		}
 
 		if (showxml)
 			printf("</document>\n");

mupdf/fitz/doc_outline.c
@@ -1,0 +1,51 @@
+#include "fitz.h"
+
+void
+fz_free_outline(fz_outline *outline)
+{
+	while (outline)
+	{
+		fz_outline *next = outline->next;
+		fz_free_outline(outline->down);
+		fz_free(outline->title);
+		/* SumatraPDF: extended outline actions */
+		if (outline->free_data) outline->free_data(outline->data);
+		fz_free(outline);
+		outline = next;
+	}
+}
+
+void
+fz_debug_outline_xml(fz_outline *outline, int level)
+{
+	while (outline)
+	{
+		printf("<outline title=\"%s\" page=\"%d\"", outline->title, outline->page);
+		if (outline->down)
+		{
+			printf(">\n");
+			fz_debug_outline_xml(outline->down, level + 1);
+			printf("</outline>\n");
+		}
+		else
+		{
+			printf(" />\n");
+		}
+		outline = outline->next;
+	}
+}
+
+void
+fz_debug_outline(fz_outline *outline, int level)
+{
+	int i;
+	while (outline)
+	{
+		for (i = 0; i < level; i++)
+			putchar('\t');
+		printf("%s\t%d\n", outline->title, outline->page);
+		if (outline->down)
+			fz_debug_outline(outline->down, level + 1);
+		outline = outline->next;
+	}
+}

commit 86e783dc3649d480ad211f5a1ecfe6819f2ccd98

fix ToC regression from r4662

mupdf/pdf/pdf_outline.c
@@ -34,7 +34,7 @@ pdf_load_outline_imp(pdf_xref *xref, fz_obj *dict)
 		node->title = pdf_to_utf8(obj);
 
 	/* SumatraPDF: support expansion states */
-	node->is_open = fz_to_int(obj) >= 0;
+	node->is_open = fz_to_int(fz_dict_gets(dict, "Count")) >= 0;
 	/* SumatraPDF: extended outline actions */
 	node->data = node->free_data = NULL;
 