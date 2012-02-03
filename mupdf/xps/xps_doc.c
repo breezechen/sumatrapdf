breezechen/sumatrapdf

commit 443e0362991781853a4a66e837aac5c5dccb307e

merge latest MuPDF update
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5405 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/xps/xps_doc.c
@@ -69,6 +69,79 @@ xps_add_fixed_document(xps_document *doc, char *name)
 	}
 }
 
+void
+xps_add_link(xps_document *doc, fz_rect area, char *base_uri, char *target_uri)
+{
+	int len;
+	char *buffer = NULL;
+	char *uri;
+	xps_target *target;
+	fz_link_dest dest;
+	fz_link *link;
+	fz_context *ctx = doc->ctx;
+
+	fz_var(buffer);
+
+	if (doc->current_page == NULL || doc->current_page->links_resolved)
+		return;
+
+	fz_try(ctx)
+	{
+		len = 2 + (base_uri ? strlen(base_uri) : 0) +
+			(target_uri ? strlen(target_uri) : 0);
+		buffer = fz_malloc(doc->ctx, len);
+		xps_resolve_url(buffer, base_uri, target_uri, len);
+		if (xps_url_is_remote(buffer))
+		{
+			dest.kind = FZ_LINK_URI;
+			dest.ld.uri.is_map = 0;
+			dest.ld.uri.uri = buffer;
+			buffer = NULL;
+		}
+		else
+		{
+			uri = buffer;
+
+			/* FIXME: This won't work for remote docs */
+			/* Skip until we find the fragment marker */
+			while (*uri && *uri != '#')
+				uri++;
+			if (*uri == '#')
+				uri++;
+
+			for (target = doc->target; target; target = target->next)
+				if (!strcmp(target->name, uri))
+					break;
+
+			if (target == NULL)
+				break;
+
+			dest.kind = FZ_LINK_GOTO;
+			dest.ld.gotor.flags = 0;
+			dest.ld.gotor.lt.x = 0;
+			dest.ld.gotor.lt.y = 0;
+			dest.ld.gotor.rb.x = 0;
+			dest.ld.gotor.rb.y = 0;
+			dest.ld.gotor.page = target->page;
+			dest.ld.gotor.file_spec = NULL;
+			dest.ld.gotor.new_window = 0;
+		}
+
+		link = fz_new_link(doc->ctx, area, dest);
+		link->next = doc->current_page->links;
+		doc->current_page->links = link;
+	}
+	fz_always(ctx)
+	{
+		fz_free(doc->ctx, buffer);
+	}
+	fz_catch(ctx)
+	{
+		fz_rethrow(ctx);
+	}
+}
+
+
 static void
 xps_add_fixed_page(xps_document *doc, char *name, int width, int height)
 {
@@ -84,6 +157,8 @@ xps_add_fixed_page(xps_document *doc, char *name, int width, int height)
 	page->number = doc->page_count++;
 	page->width = width;
 	page->height = height;
+	page->links = NULL;
+	page->links_resolved = 0;
 	page->root = NULL;
 	page->next = NULL;
 
@@ -201,7 +276,7 @@ xps_parse_metadata_imp(xps_document *doc, xml_element *item, xps_fixdoc *fixdoc)
 			if (target && type)
 			{
 				char tgtbuf[1024];
-				xps_absolute_path(tgtbuf, doc->base_uri, target, sizeof tgtbuf);
+				xps_resolve_url(tgtbuf, doc->base_uri, target, sizeof tgtbuf);
 				if (!strcmp(type, REL_START_PART))
 					doc->start_part = fz_strdup(doc->ctx, tgtbuf);
 				if (!strcmp(type, REL_DOC_STRUCTURE) && fixdoc)
@@ -218,7 +293,7 @@ xps_parse_metadata_imp(xps_document *doc, xml_element *item, xps_fixdoc *fixdoc)
 			if (source)
 			{
 				char srcbuf[1024];
-				xps_absolute_path(srcbuf, doc->base_uri, source, sizeof srcbuf);
+				xps_resolve_url(srcbuf, doc->base_uri, source, sizeof srcbuf);
 				xps_add_fixed_document(doc, srcbuf);
 			}
 		}
@@ -233,7 +308,7 @@ xps_parse_metadata_imp(xps_document *doc, xml_element *item, xps_fixdoc *fixdoc)
 			if (source)
 			{
 				char srcbuf[1024];
-				xps_absolute_path(srcbuf, doc->base_uri, source, sizeof srcbuf);
+				xps_resolve_url(srcbuf, doc->base_uri, source, sizeof srcbuf);
 				xps_add_fixed_page(doc, srcbuf, width, height);
 			}
 		}
@@ -400,6 +475,7 @@ xps_load_page(xps_document *doc, int number)
 	{
 		if (n == number)
 		{
+			doc->current_page = page;
 			if (!page->root)
 				xps_load_fixed_page(doc, page);
 			return page;
@@ -427,5 +503,81 @@ xps_free_page(xps_document *doc, xps_page *page)
 	/* only free the XML contents */
 	if (page->root)
 		xml_free_element(doc->ctx, page->root);
+	fz_free_link(doc->ctx, page->links);
 	page->root = NULL;
+}
+
+/* SumatraPDF: extract page bounds without parsing the entire page content */
+static xps_page *
+xps_get_page(xps_document *doc, int number)
+{
+	int n;
+	xps_page *page = doc->first_page;
+	for (n = 0; n < number && page; n++)
+		page = page->next;
+	return page;
+}
+
+fz_rect
+xps_bound_page_quick_and_dirty(xps_document *doc, int number)
+{
+	fz_rect bounds = fz_empty_rect;
+	byte *end = NULL;
+	xml_element *root;
+
+	xps_page *page = xps_get_page(doc, number);
+	xps_part *part = page ? xps_read_part(doc, page->name) : NULL;
+	if (!part)
+		return fz_empty_rect;
+
+	if (part->data[0] == 0xFF && part->data[1] == 0xFE)
+	{
+		wchar_t *start = wcsstr((wchar_t *)part->data, L"<FixedPage");
+		if (start)
+			end = (byte *)wcschr(start, '>');
+		if (end)
+			end += 2;
+	}
+	else
+	{
+		const char *start = strstr((char *)part->data, "<FixedPage");
+		if (start)
+			end = (byte *)strchr(start, '>');
+		if (end)
+			// xml_parse_document ignores the length argument for UTF-8 data
+			*(++end) = '\0';
+	}
+	if (!end)
+	{
+		xps_free_part(doc, part);
+		return fz_empty_rect;
+	}
+
+	// we depend on the parser not validating its input (else we'd
+	// have to append a closing "</FixedPage>" to the byte data)
+	fz_try(doc->ctx)
+	{
+		root = xml_parse_document(doc->ctx, part->data, (int)(end - part->data));
+	}
+	fz_always(doc->ctx)
+	{
+		xps_free_part(doc, part);
+	}
+	fz_catch(doc->ctx)
+	{
+		fz_rethrow(doc->ctx);
+	}
+
+	if (!strcmp(xml_tag(root), "FixedPage"))
+	{
+		char *value = xml_att(root, "Width");
+		if (value)
+			bounds.x1 = fz_atof(value) * 72.0f / 96.0f;
+		value = xml_att(root, "Height");
+		if (value)
+			bounds.y1 = fz_atof(value) * 72.0f / 96.0f;
+	}
+	xml_free_element(doc->ctx, root);
+
+	return bounds;
 }

src/PdfEngine.cpp
@@ -2461,7 +2461,7 @@ protected:
 
     Vec<XpsPageRun*>runCache; // ordered most recently used first
     XpsPageRun    * CreatePageRun(xps_page *page, fz_display_list *list);
-    XpsPageRun    * GetPageRun(xps_page *page, bool tryOnly=false, int firstForPage=0);
+    XpsPageRun    * GetPageRun(xps_page *page, bool tryOnly=false);
     bool            RunPage(xps_page *page, fz_device *dev, fz_matrix ctm,
                             fz_bbox clipbox=fz_infinite_bbox, bool cacheRun=true);
     void            DropPageRun(XpsPageRun *run, bool forceRemove=false);
@@ -2472,7 +2472,6 @@ protected:
 
     RectD         * _mediaboxes;
     fz_outline    * _outline;
-    fz_link      ** _links;
     fz_obj        * _info;
     fz_rect      ** imageRects;
 };
@@ -2550,7 +2549,7 @@ public:
 };
 
 CXpsEngine::CXpsEngine() : _fileName(NULL), _doc(NULL), _pages(NULL), _mediaboxes(NULL),
-    _outline(NULL), _links(NULL), _info(NULL), imageRects(NULL)
+    _outline(NULL), _info(NULL), imageRects(NULL)
 {
     InitializeCriticalSection(&_pagesAccess);
     InitializeCriticalSection(&ctxAccess);
@@ -2564,19 +2563,10 @@ CXpsEngine::~CXpsEngine()
     EnterCriticalSection(&ctxAccess);
 
     if (_pages) {
-        for (int i = 0; i < PageCount(); i++) {
-            if (_pages[i])
-                xps_free_page(_doc, _pages[i]);
-        }
+        // xps_pages are freed by xps_close_document -> xps_free_page_list
+        assert(_doc);
         free(_pages);
     }
-    if (_links) {
-        for (int i = 0; i < PageCount(); i++)
-            if (_links[i])
-                fz_free_link(ctx, _links[i]);
-        free(_links);
-    }
-
     if (_outline)
         fz_free_outline(ctx, _outline);
     if (_mediaboxes)
@@ -2694,11 +2684,10 @@ bool CXpsEngine::LoadFromStream(fz_stream *stm)
     }
 
     _pages = SAZA(xps_page *, PageCount());
-    _links = SAZA(fz_link *, PageCount());
     _mediaboxes = new RectD[PageCount()];
     imageRects = SAZA(fz_rect *, PageCount());
 
-    if (!_pages || !_links || !_mediaboxes || !imageRects)
+    if (!_pages || !_mediaboxes || !imageRects)
         return false;
 
     fz_try(ctx) {
@@ -2781,7 +2770,7 @@ XpsPageRun *CXpsEngine::CreatePageRun(xps_page *page, fz_display_list *list)
     return (XpsPageRun *)_memdup(&newRun);
 }
 
-XpsPageRun *CXpsEngine::GetPageRun(xps_page *page, bool tryOnly, int firstForPage)
+XpsPageRun *CXpsEngine::GetPageRun(xps_page *page, bool tryOnly)
 {
     ScopedCritSec scope(&_pagesAccess);
 
@@ -2815,8 +2804,6 @@ XpsPageRun *CXpsEngine::GetPageRun(xps_page *page, bool tryOnly, int firstForPag
         fz_link root = { 0 };
         fz_var(list);
         fz_var(dev);
-        if (firstForPage)
-            _doc->link_root = &root;
         fz_try(ctx) {
             list = fz_new_display_list(ctx);
             dev = fz_new_list_device(ctx, list);
@@ -2828,16 +2815,10 @@ XpsPageRun *CXpsEngine::GetPageRun(xps_page *page, bool tryOnly, int firstForPag
             list = NULL;
         }
         fz_free_device(dev);
-        _doc->link_root = NULL;
 
         if (list) {
             result = CreatePageRun(page, list);
             runCache.InsertAt(0, result);
-
-            if (firstForPage) {
-                assert(!_links[firstForPage-1]);
-                _links[firstForPage-1] = root.next;
-            }
         }
     }
     else if (result && result != runCache.At(0)) {
@@ -2899,67 +2880,6 @@ void CXpsEngine::DropPageRun(XpsPageRun *run, bool forceRemove)
     }
 }
 
-// <FixedPage xmlns="http://schemas.microsoft.com/xps/2005/06" Width="816" Height="1056" xml:lang="en-US">
-
-// MuPDF doesn't allow partial parsing of XML content, so try to
-// extract a page's root element manually before feeding it to the parser
-static RectD xps_extract_mediabox_quick_and_dirty(xps_document *doc, int pageNo)
-{
-    xps_part *part = NULL;
-    for (xps_page *page = doc->first_page; page && !part; page = page->next)
-        if (--pageNo == 0)
-            part = xps_read_part(doc, page->name);
-    if (!part)
-        return RectD();
-
-    byte *end = NULL;
-    if (0xFF == part->data[0] && 0xFE == part->data[1]) {
-        const WCHAR *start = str::Find((WCHAR *)part->data, L"<FixedPage");
-        if (start)
-            end = (byte *)str::FindChar(start, '>');
-        if (end)
-            end += 2;
-    }
-    else {
-        const char *start = str::Find((char *)part->data, "<FixedPage");
-        if (start)
-            end = (byte *)str::FindChar(start, '>');
-        if (end)
-            // xml_parse_document ignores the length argument for UTF-8 data
-            *(++end) = '\0';
-    }
-    if (!end) {
-        xps_free_part(doc, part);
-        return RectD();
-    }
-
-    // we depend on the parser not validating its input (else we'd
-    // have to append a closing "</FixedPage>" to the byte data)
-    xml_element *root;
-    fz_try(doc->ctx) {
-        root = xml_parse_document(doc->ctx, part->data, (int)(end - part->data));
-    }
-    fz_always(doc->ctx) {
-        xps_free_part(doc, part);
-    }
-    fz_catch(doc->ctx) {
-        fz_rethrow(doc->ctx);
-    }
-
-    RectD rect;
-    if (str::Eq(xml_tag(root), "FixedPage")) {
-        char *width = xml_att(root, "Width");
-        if (width)
-            rect.dx = atof(width) * 72 / 96;
-        char *height = xml_att(root, "Height");
-        if (height)
-            rect.dy = atof(height) * 72 / 96;
-    }
-
-    xml_free_element(doc->ctx, root);
-    return rect;
-}
-
 RectD CXpsEngine::PageMediabox(int pageNo)
 {
     assert(1 <= pageNo && pageNo <= PageCount());
@@ -2974,7 +2894,7 @@ RectD CXpsEngine::PageMediabox(int pageNo)
     if (!page) {
         ScopedCritSec scope(&ctxAccess);
         fz_try(ctx) {
-            mbox = xps_extract_mediabox_quick_and_dirty(_doc, pageNo);
+            mbox = fz_rect_to_RectD(xps_bound_page_quick_and_dirty(_doc, pageNo-1));
         }
         fz_catch(ctx) { }
         if (!mbox.IsEmpty()) {
@@ -3227,8 +3147,12 @@ TCHAR *CXpsEngine::GetProperty(char *name)
 
 PageElement *CXpsEngine::GetElementAtPos(int pageNo, PointD pt)
 {
+    xps_page *page = GetXpsPage(pageNo, true);
+    if (!page)
+        return NULL;
+
     fz_point p = { (float)pt.x, (float)pt.y };
-    for (fz_link *link = _links[pageNo-1]; link; link = link->next)
+    for (fz_link *link = page->links; link; link = link->next)
         if (fz_is_pt_in_rect(link->rect, p))
             return new XpsLink(this, &link->dest, link->rect, pageNo);
 
@@ -3243,6 +3167,10 @@ PageElement *CXpsEngine::GetElementAtPos(int pageNo, PointD pt)
 
 Vec<PageElement *> *CXpsEngine::GetElements(int pageNo)
 {
+    xps_page *page = GetXpsPage(pageNo, true);
+    if (!page)
+        return NULL;
+
     // since all elements lists are in last-to-first order, append
     // item types in inverse order and reverse the whole list at the end
     Vec<PageElement *> *els = new Vec<PageElement *>();
@@ -3255,7 +3183,7 @@ Vec<PageElement *> *CXpsEngine::GetElements(int pageNo)
         }
     }
 
-    for (fz_link *link = _links[pageNo-1]; link; link = link->next) {
+    for (fz_link *link = page->links; link; link = link->next) {
         els->Append(new XpsLink(this, &link->dest, link->rect, pageNo));
     }
 
@@ -3267,11 +3195,10 @@ void CXpsEngine::LinkifyPageText(xps_page *page, int pageNo)
 {
     // make MuXPS extract all links and named destinations from the page
     assert(!GetPageRun(page, true));
-    XpsPageRun *run = GetPageRun(page, false, pageNo);
+    XpsPageRun *run = GetPageRun(page);
     assert(run);
     if (run)
         DropPageRun(run);
-    _links[pageNo-1] = FixupPageLinks(_links[pageNo-1]);
 
     RectI *coords;
     TCHAR *pageText = ExtractPageText(page, _T("\n"), &coords, true);
@@ -3281,7 +3208,7 @@ void CXpsEngine::LinkifyPageText(xps_page *page, int pageNo)
     LinkRectList *list = LinkifyText(pageText, coords);
     for (size_t i = 0; i < list->links.Count(); i++) {
         bool overlaps = false;
-        for (fz_link *next = _links[pageNo-1]; next && !overlaps; next = next->next)
+        for (fz_link *next = page->links; next && !overlaps; next = next->next)
             overlaps = fz_calc_overlap(next->rect, list->coords.At(i)) >= 0.25f;
         if (!overlaps) {
             ScopedMem<char> uri(str::conv::ToUtf8(list->links.At(i)));
@@ -3290,8 +3217,8 @@ void CXpsEngine::LinkifyPageText(xps_page *page, int pageNo)
             ld.ld.uri.uri = fz_strdup(ctx, uri);
             // add links in top-to-bottom order (i.e. last-to-first)
             fz_link *link = fz_new_link(ctx, list->coords.At(i), ld);
-            link->next = _links[pageNo-1];
-            _links[pageNo-1] = link;
+            link->next = page->links;
+            page->links = link;
         }
     }
 

mupdf/Makefile
@@ -156,8 +156,8 @@ mandir ?= $(prefix)/share/man
 install: $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(PDF_APPS) $(XPS_APPS) $(MUPDF)
 	install -d $(bindir) $(libdir) $(incdir) $(mandir)/man1
 	install $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(libdir)
-	install fitz/fitz.h pdf/mupdf.h xps/muxps.h $(incdir)
-	install $(PDF_APPS) $(XPS_APPS) $(MUPDF) $(bindir)
+	install fitz/memento.h fitz/fitz.h pdf/mupdf.h xps/muxps.h $(incdir)
+	install cbz/mucbz.h $(PDF_APPS) $(XPS_APPS) $(MUPDF) $(bindir)
 	install $(wildcard apps/man/*.1) $(mandir)/man1
 
 # --- Clean and Default ---

mupdf/apps/pdfapp.c
@@ -367,13 +367,15 @@ static void pdfapp_loadpage_xps(pdfapp_t *app)
 	}
 
 	app->page_bbox = xps_bound_page(app->xps, page);
-	app->page_links = NULL;
 
 	/* Create display list */
 	app->page_list = fz_new_display_list(app->ctx);
 	mdev = fz_new_list_device(app->ctx, app->page_list);
 	xps_run_page(app->xps, page, mdev, fz_identity, NULL);
 	fz_free_device(mdev);
+
+	app->page_links = page->links;
+	page->links = NULL;
 
 	xps_free_page(app->xps, page);
 }

mupdf/fitz/image_tiff.c
@@ -313,6 +313,8 @@ fz_expand_tiff_colormap(struct tiff *tiff)
 	tiff->samplesperpixel += 2;
 	tiff->bitspersample = 8;
 	tiff->stride = stride;
+	/* SumatraPDF: fix memory leak */
+	fz_free(tiff->ctx, tiff->samples);
 	tiff->samples = samples;
 }
 

mupdf/ios/main.m
@@ -874,7 +874,7 @@ static UIImage *renderTile(struct document *doc, int number, CGSize screenSize, 
 			outline = [[MuOutlineController alloc] initWithTarget: self titles: titles pages: pages];
 		[titles release];
 		[pages release];
-		fz_free_outline(root);
+		fz_free_outline(ctx, root);
 	}
 
 	return self;

mupdf/libmupdf.def
@@ -473,7 +473,8 @@ EXPORTS
 ; MuXPS exports
 
 	xps_strcasecmp
-	xps_absolute_path
+	xps_resolve_url
+	xps_url_is_remote
 	xml_parse_document
 	xml_next
 	xml_down
@@ -493,8 +494,10 @@ EXPORTS
 	xps_load_page
 	xps_bound_page
 	xps_free_page
+	xps_bound_page_quick_and_dirty
 	xps_load_outline
 	xps_find_link_target
+	xps_add_link
 	xps_find_link_target_obj
 	xps_count_font_encodings
 	xps_identify_font_encoding

mupdf/xps/muxps.h
@@ -32,7 +32,8 @@ typedef struct xps_document_s xps_document;
  */
 
 int xps_strcasecmp(char *a, char *b);
-void xps_absolute_path(char *output, char *base_uri, char *path, int output_size);
+void xps_resolve_url(char *output, char *base_uri, char *path, int output_size);
+int xps_url_is_remote(char *path);
 
 /*
  * XML document model
@@ -91,6 +92,8 @@ struct xps_page_s
 	int width;
 	int height;
 	xml_element *root;
+	int links_resolved;
+	fz_link *links;
 	xps_page *next;
 };
 
@@ -110,10 +113,13 @@ int xps_count_pages(xps_document *doc);
 xps_page *xps_load_page(xps_document *doc, int number);
 fz_rect xps_bound_page(xps_document *doc, xps_page *page);
 void xps_free_page(xps_document *doc, xps_page *page);
+/* SumatraPDF: extract page bounds without parsing the entire page content */
+fz_rect xps_bound_page_quick_and_dirty(xps_document *doc, int number);
 
 fz_outline *xps_load_outline(xps_document *doc);
 
 int xps_find_link_target(xps_document *doc, char *target_uri);
+void xps_add_link(xps_document *doc, fz_rect area, char *base_uri, char *target_uri);
 /* SumatraPDF: extended link support */
 xps_target *xps_find_link_target_obj(xps_document *doc, char *target_uri);
 
@@ -254,8 +260,9 @@ struct xps_document_s
 	fz_cookie *cookie;
 	fz_device *dev;
 
-	/* SumatraPDF: set to non-NULL for link extraction */
-	fz_link *link_root;
+	/* Current page we are loading */
+	xps_page *current_page;
+
 	/* SumatraPDF: better canvas bounds estimates for links/targets */
 	struct {
 		fz_link *link;
@@ -275,7 +282,7 @@ char *xps_get_real_params(char *s, int num, float *x);
 char *xps_get_point(char *s_in, float *x, float *y);
 
 /* SumatraPDF: extended link support */
-void xps_extract_anchor_info(xps_document *doc, xml_element *node, fz_rect rect, int step);
+void xps_extract_anchor_info(xps_document *doc, fz_rect rect, char *target_uri, char *anchor_name, int step);
 /* SumatraPDF: extract document properties (hacky) */
 fz_obj *xps_extract_doc_props(xps_document *doc);
 

commit 08bf74d87a0842184c2255018f5be0322a4fa051

fix reuse-after-free crash introduced with r5405

makefile.msvc
@@ -620,9 +620,10 @@ $(OS)\TableOfContents.obj: src\TextSelection.h src\translations.h src\utils\Allo
 $(OS)\TableOfContents.obj: src\utils\BaseUtil.h src\utils\GeomUtil.h src\utils\Scoped.h
 $(OS)\TableOfContents.obj: src\utils\StrUtil.h src\utils\Vec.h src\utils\WinUtil.h
 $(OS)\TableOfContents.obj: src\WindowInfo.h
-$(OS)\Tester.obj: src\MobiDoc.h src\utils\Allocator.h src\utils\BaseUtil.h
-$(OS)\Tester.obj: src\utils\DirIter.h src\utils\FileUtil.h src\utils\GdiPlusUtil.h
-$(OS)\Tester.obj: src\utils\Scoped.h src\utils\StrUtil.h src\utils\Vec.h
+$(OS)\Tester.obj: src\MobiDoc.h src\PageLayout.h src\utils\Allocator.h
+$(OS)\Tester.obj: src\utils\BaseUtil.h src\utils\DirIter.h src\utils\FileUtil.h
+$(OS)\Tester.obj: src\utils\GdiPlusUtil.h src\utils\GeomUtil.h src\utils\Scoped.h
+$(OS)\Tester.obj: src\utils\StrUtil.h src\utils\Vec.h src\utils\WinUtil.h
 $(OS)\TextSearch.obj: src\BaseEngine.h src\TextSearch.h src\TextSelection.h
 $(OS)\TextSearch.obj: src\utils\Allocator.h src\utils\BaseUtil.h src\utils\GeomUtil.h
 $(OS)\TextSearch.obj: src\utils\StrUtil.h src\utils\Vec.h

mupdf/xps/xps_doc.c
@@ -505,6 +505,9 @@ xps_free_page(xps_document *doc, xps_page *page)
 		xml_free_element(doc->ctx, page->root);
 	fz_free_link(doc->ctx, page->links);
 	page->root = NULL;
+	/* SumatraPDF: make sure that links can be reloaded */
+	page->links = NULL;
+	page->links_resolved = 0;
 }
 
 /* SumatraPDF: extract page bounds without parsing the entire page content */

src/PdfEngine.cpp
@@ -2410,7 +2410,9 @@ public:
 
     virtual unsigned char *GetFileData(size_t *cbCount);
     virtual TCHAR * ExtractPageText(int pageNo, TCHAR *lineSep, RectI **coords_out=NULL,
-                                    RenderTarget target=Target_View);
+                                    RenderTarget target=Target_View) {
+        return ExtractPageText(GetXpsPage(pageNo), lineSep, coords_out);
+    }
     virtual bool IsImagePage(int pageNo);
     virtual TCHAR *GetProperty(char *name);
 
@@ -2720,9 +2722,12 @@ xps_page *CXpsEngine::GetXpsPage(int pageNo, bool failIfBusy)
         ScopedCritSec ctxScope(&ctxAccess);
         fz_var(page);
         fz_try(ctx) {
+            // caution: two calls to xps_load_page return the
+            // same xps_page object (without reference counting)
             page = xps_load_page(_doc, pageNo - 1);
             _pages[pageNo-1] = page;
             LinkifyPageText(page, pageNo);
+            assert(page->links_resolved);
         }
         fz_catch(ctx) { }
     }
@@ -3089,32 +3094,6 @@ TCHAR *CXpsEngine::ExtractPageText(xps_page *page, TCHAR *lineSep, RectI **coord
     fz_free_text_span(ctx, text);
 
     return content;
-}
-
-TCHAR *CXpsEngine::ExtractPageText(int pageNo, TCHAR *lineSep, RectI **coords_out, RenderTarget target)
-{
-    xps_page *page = GetXpsPage(pageNo, true);
-    if (page)
-        return ExtractPageText(page, lineSep, coords_out);
-
-    EnterCriticalSection(&ctxAccess);
-    fz_var(page);
-    fz_try(ctx) {
-        page = xps_load_page(_doc, pageNo - 1);
-    }
-    fz_catch(ctx) {
-        LeaveCriticalSection(&ctxAccess);
-        return NULL;
-    }
-    LeaveCriticalSection(&ctxAccess);
-
-    TCHAR *result = ExtractPageText(page, lineSep, coords_out);
-
-    EnterCriticalSection(&ctxAccess);
-    xps_free_page(_doc, page);
-    LeaveCriticalSection(&ctxAccess);
-
-    return result;
 }
 
 unsigned char *CXpsEngine::GetFileData(size_t *cbCount)