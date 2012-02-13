breezechen/sumatrapdf

commit 2dfa20ff7dd7cb635974a5c964a9e8907bc4970e

merge latest mupdf changes
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5500 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/fitz/fitz.h
@@ -115,6 +115,7 @@ typedef struct fz_error_context_s fz_error_context;
 typedef struct fz_warn_context_s fz_warn_context;
 typedef struct fz_font_context_s fz_font_context;
 typedef struct fz_aa_context_s fz_aa_context;
+typedef struct fz_locks_context_s fz_locks_context;
 typedef struct fz_store_s fz_store;
 typedef struct fz_glyph_cache_s fz_glyph_cache;
 typedef struct fz_context_s fz_context;
@@ -125,8 +126,6 @@ struct fz_alloc_context_s
 	void *(*malloc)(void *, unsigned int);
 	void *(*realloc)(void *, void *, unsigned int);
 	void (*free)(void *, void *);
-	void (*lock)(void *);
-	void (*unlock)(void *);
 };
 
 /* Default allocator */
@@ -372,6 +371,7 @@ void fz_flush_warnings(fz_context *ctx);
 struct fz_context_s
 {
 	fz_alloc_context *alloc;
+	fz_locks_context *locks;
 	fz_error_context *error;
 	fz_warn_context *warn;
 	fz_font_context *font;
@@ -380,26 +380,84 @@ struct fz_context_s
 	fz_glyph_cache *glyph_cache;
 };
 
-fz_context *fz_new_context(fz_alloc_context *alloc, unsigned int max_store);
+fz_context *fz_new_context(fz_alloc_context *alloc, fz_locks_context *locks, unsigned int max_store);
 fz_context *fz_clone_context(fz_context *ctx);
 void fz_free_context(fz_context *ctx);
 
 void fz_new_aa_context(fz_context *ctx);
 void fz_free_aa_context(fz_context *ctx);
 
-static inline void
-fz_lock(fz_context *ctx)
+/* Locking functions
+ *
+ * MuPDF is kept deliberately free of any knowledge of particular threading
+ * systems. As such, in order for safe multi-threaded operation, we rely on
+ * callbacks to client provided functions.
+ *
+ * A client is expected to provide FZ_LOCK_MAX mutexes, and a function to
+ * lock/unlock each of them. These may be recursive mutexes, but do not have
+ * to be.
+ *
+ * If a client does not intend to use multiple threads, then it may pass
+ * NULL instead of the address of a lock structure.
+ *
+ * In order to avoid deadlocks, we have 1 simple rules internally as to how
+ * we use locks: We can never take lock n when we already hold any lock i,
+ * where 0 <= i <= n. In order to verify this, we have some debugging code
+ * built in, that is enabled by defining FITZ_DEBUG_LOCKING.
+ */
+
+#if defined(MEMENTO) || defined(DEBUG)
+#define FITZ_DEBUG_LOCKING
+#endif
+
+struct fz_locks_context_s
 {
-	if (ctx->alloc->lock)
-		ctx->alloc->lock(ctx->alloc->user);
+	void *user;
+	void (*lock)(void *, int);
+	void (*unlock)(void *, int);
+};
+
+enum {
+	FZ_LOCK_ALLOC = 0,
+	FZ_LOCK_STORE,
+	FZ_LOCK_FILE,
+	FZ_LOCK_GLYPHCACHE,
+	FZ_LOCK_MAX
+};
+
+/* Default locks */
+extern fz_locks_context fz_locks_default;
+
+#ifdef FITZ_DEBUG_LOCKING
+
+void fz_assert_lock_held(fz_context *ctx, int lock);
+void fz_assert_lock_not_held(fz_context *ctx, int lock);
+void fz_lock_debug_lock(fz_context *ctx, int lock);
+void fz_lock_debug_unlock(fz_context *ctx, int lock);
+
+#else
+
+#define fz_assert_lock_held(A,B) do { } while (0)
+#define fz_assert_lock_not_held(A,B) do { } while (0)
+#define fz_lock_debug_lock(A,B) do { } while (0)
+#define fz_lock_debug_unlock(A,B) do { } while (0)
+
+#endif /* !FITZ_DEBUG_LOCKING */
+
+static inline void
+fz_lock(fz_context *ctx, int lock)
+{
+	fz_lock_debug_lock(ctx, lock);
+	ctx->locks->lock(ctx->locks->user, lock);
 }
 
 static inline void
-fz_unlock(fz_context *ctx)
+fz_unlock(fz_context *ctx, int lock)
 {
-	if (ctx->alloc->unlock)
-		ctx->alloc->unlock(ctx->alloc->user);
+	fz_lock_debug_unlock(ctx, lock);
+	ctx->locks->unlock(ctx->locks->user, lock);
 }
+
 
 /*
  * Basic runtime and utility functions
@@ -460,7 +518,7 @@ void fz_empty_hash(fz_context *ctx, fz_hash_table *table);
 void fz_free_hash(fz_context *ctx, fz_hash_table *table);
 
 void *fz_hash_find(fz_context *ctx, fz_hash_table *table, void *key);
-void fz_hash_insert(fz_context *ctx, fz_hash_table *table, void *key, void *val);
+void *fz_hash_insert(fz_context *ctx, fz_hash_table *table, void *key, void *val);
 void fz_hash_remove(fz_context *ctx, fz_hash_table *table, void *key);
 
 int fz_hash_len(fz_context *ctx, fz_hash_table *table);
@@ -791,6 +849,7 @@ struct fz_stream_s
 	int pos;
 	int avail;
 	int bits;
+	int locked;
 	unsigned char *bp, *rp, *wp, *ep;
 	void *state;
 	int (*read)(fz_stream *stm, unsigned char *buf, int len);
@@ -807,6 +866,7 @@ fz_stream *fz_open_file_w(fz_context *ctx, const wchar_t *filename); /* only on 
 fz_stream *fz_open_buffer(fz_context *ctx, fz_buffer *buf);
 fz_stream *fz_open_memory(fz_context *ctx, unsigned char *data, int len);
 void fz_close(fz_stream *stm);
+void fz_lock_stream(fz_stream *stm);
 /* SumatraPDF: allow to clone a stream */
 fz_stream *fz_clone_stream(fz_context *ctx, fz_stream *stm);
 
@@ -1206,6 +1266,7 @@ void fz_debug_path(fz_context *ctx, fz_path *, int indent);
  */
 
 void fz_new_glyph_cache_context(fz_context *ctx);
+fz_glyph_cache *fz_glyph_cache_keep(fz_context *ctx);
 void fz_free_glyph_cache_context(fz_context *ctx);
 
 fz_pixmap *fz_render_ft_glyph(fz_context *ctx, fz_font *font, int cid, fz_matrix trm);
@@ -1474,9 +1535,6 @@ void fz_run_display_list(fz_display_list *list, fz_device *dev, fz_matrix ctm, f
 /*
  * Plotting functions.
  */
-
-void fz_accelerate(void);
-void fz_accelerate_arch(void);
 
 void fz_decode_tile(fz_pixmap *pix, float *decode);
 void fz_decode_indexed_tile(fz_pixmap *pix, float *decode, int maxval);

mupdf/pdf/pdf_stream.c
@@ -225,6 +225,7 @@ pdf_open_filter(fz_stream *chain, pdf_document *xref, fz_obj *stmobj, int num, i
 	else if (fz_array_len(filters) > 0)
 		chain = build_filter_chain(chain, xref, filters, params, num, gen);
 
+	fz_lock_stream(chain);
 	return chain;
 }
 
@@ -276,6 +277,7 @@ pdf_open_raw_stream(pdf_document *xref, int num, int gen)
 		fz_throw(xref->ctx, "object is not a stream");
 
 	stm = pdf_open_raw_filter(xref->file, xref, x->obj, num, gen);
+	fz_lock_stream(stm);
 	fz_seek(xref->file, x->stm_ofs, 0);
 	return stm;
 }
@@ -378,9 +380,6 @@ pdf_load_stream(pdf_document *xref, int num, int gen)
 
 	fz_var(buf);
 
-	stm = pdf_open_stream(xref, num, gen);
-	/* RJW: "cannot open stream (%d %d R)", num, gen */
-
 	dict = pdf_load_object(xref, num, gen);
 	/* RJW: "cannot load stream dictionary (%d %d R)", num, gen */
 
@@ -393,17 +392,21 @@ pdf_load_stream(pdf_document *xref, int num, int gen)
 
 	fz_drop_obj(dict);
 
+	stm = pdf_open_stream(xref, num, gen);
+	/* RJW: "cannot open stream (%d %d R)", num, gen */
+
 	fz_try(ctx)
 	{
-		/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692260 */
-		buf = fz_read_all2(stm, len, 0);
+		buf = fz_read_all(stm, len);
+	}
+	fz_always(ctx)
+	{
+		fz_close(stm);
 	}
 	fz_catch(ctx)
 	{
-		fz_close(stm);
 		fz_throw(ctx, "cannot read raw stream (%d %d R)", num, gen);
 	}
 
-	fz_close(stm);
 	return buf;
 }

mupdf/Makefile
@@ -56,19 +56,16 @@ $(OUT)/%.o : xps/%.c fitz/fitz.h xps/muxps.h | $(OUT)
 	$(CC_CMD)
 $(OUT)/%.o : cbz/%.c fitz/fitz.h cbz/mucbz.h | $(OUT)
 	$(CC_CMD)
-$(OUT)/%.o : apps/%.c fitz/fitz.h pdf/mupdf.h xps/muxps.h | $(OUT)
+$(OUT)/%.o : apps/%.c fitz/fitz.h pdf/mupdf.h xps/muxps.h cbz/mucbz.h | $(OUT)
 	$(CC_CMD)
 $(OUT)/%.o : scripts/%.c | $(OUT)
 	$(CC_CMD)
 
 .PRECIOUS : $(OUT)/%.o # Keep intermediates from chained rules
 
-# --- Fitz, MuPDF, MuXPS and MuCBZ libraries ---
+# --- Fitz, MuPDF, MuXPS and MuCBZ library ---
 
 FITZ_LIB := $(OUT)/libfitz.a
-MUPDF_LIB := $(OUT)/libmupdf.a
-MUXPS_LIB := $(OUT)/libmuxps.a
-MUCBZ_LIB := $(OUT)/libmucbz.a
 
 FITZ_SRC := $(notdir $(wildcard fitz/*.c draw/*.c))
 FITZ_SRC := $(filter-out draw_simple_scale.c, $(FITZ_SRC))
@@ -77,11 +74,11 @@ MUXPS_SRC := $(notdir $(wildcard xps/*.c))
 MUCBZ_SRC := $(notdir $(wildcard cbz/*.c))
 
 $(FITZ_LIB) : $(addprefix $(OUT)/, $(FITZ_SRC:%.c=%.o))
-$(MUPDF_LIB) : $(addprefix $(OUT)/, $(MUPDF_SRC:%.c=%.o))
-$(MUXPS_LIB) : $(addprefix $(OUT)/, $(MUXPS_SRC:%.c=%.o))
-$(MUCBZ_LIB) : $(addprefix $(OUT)/, $(MUCBZ_SRC:%.c=%.o))
+$(FITZ_LIB) : $(addprefix $(OUT)/, $(MUPDF_SRC:%.c=%.o))
+$(FITZ_LIB) : $(addprefix $(OUT)/, $(MUXPS_SRC:%.c=%.o))
+$(FITZ_LIB) : $(addprefix $(OUT)/, $(MUCBZ_SRC:%.c=%.o))
 
-libs: $(MUCBZ_LIB) $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(THIRD_LIBS)
+libs: $(FITZ_LIB) $(THIRD_LIBS)
 
 # --- Generated CMAP and FONT files ---
 
@@ -130,17 +127,18 @@ $(OUT)/cmapdump.o : pdf/pdf_cmap.c pdf/pdf_cmap_parse.c
 
 PDF_APPS := $(addprefix $(OUT)/, mupdfdraw mupdfclean mupdfextract mupdfinfo mupdfshow)
 XPS_APPS := $(addprefix $(OUT)/, muxpsdraw)
-BUSY_APP := $(addprefix $(OUT)/, mubusy)
+
+$(PDF_APPS) : $(FITZ_LIB) $(THIRD_LIBS)
+$(XPS_APPS) : $(FITZ_LIB) $(THIRD_LIBS)
+
 BUSY_SRC := $(notdir $(wildcard apps/mubusy_*.c))
-BUSY_LIBS := $(addprefix $(OUT)/, $(BUSY_SRC:%.c=%.o))
+BUSY_APP := $(addprefix $(OUT)/, mubusy)
+$(BUSY_APP) : $(addprefix $(OUT)/, $(BUSY_SRC:%.c=%.o))
+$(BUSY_APP) : $(FITZ_LIB) $(THIRD_LIBS)
 
-$(PDF_APPS) : $(MUPDF_LIB) $(FITZ_LIB) $(THIRD_LIBS)
-$(XPS_APPS) : $(MUXPS_LIB) $(FITZ_LIB) $(THIRD_LIBS)
-$(BUSY_APP) : $(BUSY_LIBS) $(MUPDF_LIB) $(MUXPS_LIB) $(FITZ_LIB) $(THIRD_LIBS)
-
-MUPDF := $(OUT)/mupdf
-$(MUPDF) : $(MUCBZ_LIB) $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(THIRD_LIBS)
 ifeq "$(NOX11)" ""
+MUPDF := $(OUT)/mupdf
+$(MUPDF) : $(FITZ_LIB) $(THIRD_LIBS)
 $(MUPDF) : $(addprefix $(OUT)/, x11_main.o x11_image.o pdfapp.o)
 	$(LINK_CMD) $(X11_LIBS)
 endif
@@ -153,11 +151,11 @@ libdir ?= $(prefix)/lib
 incdir ?= $(prefix)/include
 mandir ?= $(prefix)/share/man
 
-install: $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(PDF_APPS) $(XPS_APPS) $(MUPDF)
+install: $(FITZ_LIB) $(PDF_APPS) $(XPS_APPS) $(MUPDF)
 	install -d $(bindir) $(libdir) $(incdir) $(mandir)/man1
-	install $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(libdir)
-	install fitz/memento.h fitz/fitz.h pdf/mupdf.h xps/muxps.h $(incdir)
-	install cbz/mucbz.h $(PDF_APPS) $(XPS_APPS) $(MUPDF) $(bindir)
+	install $(FITZ_LIB) $(libdir)
+	install fitz/memento.h fitz/fitz.h pdf/mupdf.h xps/muxps.h cbz/mucbz.h $(incdir)
+	install $(PDF_APPS) $(XPS_APPS) $(MUPDF) $(bindir)
 	install $(wildcard apps/man/*.1) $(mandir)/man1
 
 # --- Clean and Default ---

mupdf/android/jni/mupdf.c
@@ -35,7 +35,6 @@ JNIEXPORT int JNICALL
 Java_com_artifex_mupdf_MuPDFCore_openFile(JNIEnv * env, jobject thiz, jstring jfilename)
 {
 	const char *filename;
-	int accelerate = 1;
 	int pages = 0;
 
 	filename = (*env)->GetStringUTFChars(env, jfilename, NULL);
@@ -45,11 +44,8 @@ Java_com_artifex_mupdf_MuPDFCore_openFile(JNIEnv * env, jobject thiz, jstring jf
 		return 0;
 	}
 
-	if (accelerate)
-		fz_accelerate();
-
 	/* 128 MB store for low memory devices. Tweak as necessary. */
-	ctx = fz_new_context(NULL, 128 << 20);
+	ctx = fz_new_context(NULL, NULL, 128 << 20);
 	if (!ctx)
 	{
 		LOGE("Failed to initialise context");

mupdf/apps/mupdfclean.c
@@ -782,7 +782,7 @@ int main(int argc, char **argv)
 	if (argc - fz_optind > 0)
 		subset = 1;
 
-	ctx = fz_new_context(NULL, FZ_STORE_UNLIMITED);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

mupdf/apps/mupdfdraw.c
@@ -49,7 +49,6 @@ static void usage(void)
 #endif
 		"\t-p -\tpassword\n"
 		"\t-r -\tresolution in dpi (default: 72)\n"
-		"\t-A\tdisable accelerated functions\n"
 		"\t-a\tsave alpha channel (only pam and png)\n"
 		"\t-b -\tnumber of bits of antialiasing (0 to 8)\n"
 		"\t-g\trender in grayscale\n"
@@ -475,14 +474,13 @@ int main(int argc, char **argv)
 {
 	char *password = "";
 	int grayscale = 0;
-	int accelerate = 1;
 	pdf_document *doc = NULL;
 	int c;
 	fz_context *ctx;
 
 	fz_var(doc);
 
-	while ((c = fz_getopt(argc, argv, "lo:p:r:R:Aab:dgmtx5G:I")) != -1)
+	while ((c = fz_getopt(argc, argv, "lo:p:r:R:ab:dgmtx5G:I")) != -1)
 	{
 		switch (c)
 		{
@@ -490,7 +488,6 @@ int main(int argc, char **argv)
 		case 'p': password = fz_optarg; break;
 		case 'r': resolution = atof(fz_optarg); break;
 		case 'R': rotation = atof(fz_optarg); break;
-		case 'A': accelerate = 0; break;
 		case 'a': savealpha = 1; break;
 		case 'b': alphabits = atoi(fz_optarg); break;
 		case 'l': showoutline++; break;
@@ -515,10 +512,7 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 
-	if (accelerate)
-		fz_accelerate();
-
-	ctx = fz_new_context(NULL, FZ_STORE_DEFAULT);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

mupdf/apps/mupdfextract.c
@@ -182,7 +182,7 @@ int main(int argc, char **argv)
 
 	infile = argv[fz_optind++];
 
-	ctx = fz_new_context(NULL, FZ_STORE_UNLIMITED);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

mupdf/apps/mupdfinfo.c
@@ -971,7 +971,7 @@ int main(int argc, char **argv)
 	if (fz_optind == argc)
 		infousage();
 
-	ctx = fz_new_context(NULL, FZ_STORE_UNLIMITED);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

commit 0777ccec007187785cbd52c25f4dc42f21d30efb

revert another accidental regression in r5500

makefile.msvc
@@ -460,11 +460,11 @@ $(OEB)\EbookTest.obj: src\utils\Allocator.h src\utils\BaseUtil.h src\utils\BitMa
 $(OEB)\EbookTest.obj: src\utils\CmdLineParser.h src\utils\DebugLog.h src\utils\FileUtil.h
 $(OEB)\EbookTest.obj: src\utils\FrameTimeoutCalculator.h src\utils\GdiPlusUtil.h src\utils\GeomUtil.h
 $(OEB)\EbookTest.obj: src\utils\HtmlPullParser.h src\utils\NoFreeAllocator.h src\utils\Scoped.h
-$(OEB)\EbookTest.obj: src\utils\StrUtil.h src\utils\Timer.h src\utils\Vec.h
-$(OEB)\EbookTest.obj: src\utils\WinUtil.h src\Version.h
+$(OEB)\EbookTest.obj: src\utils\StrUtil.h src\utils\ThreadUtil.h src\utils\Timer.h
+$(OEB)\EbookTest.obj: src\utils\Vec.h src\utils\WinUtil.h src\Version.h
 $(OEB)\EbookTestMenu.obj: src\ebooktest\EbookTestMenu.h src\ebooktest\Resource.h src\utils\BaseUtil.h
 $(OEB)\EbookTestMenu.obj: src\utils\Scoped.h src\utils\StrUtil.h
-$(OMUI)\Mui.obj: src\mui\Mui.h src\mui\mui.h src\mui\MuiBase.h
+$(OMUI)\Mui.obj: src\mui\mui.h src\mui\Mui.h src\mui\MuiBase.h
 $(OMUI)\Mui.obj: src\mui\MuiButton.h src\mui\MuiControl.h src\mui\MuiCss.h
 $(OMUI)\Mui.obj: src\mui\MuiEventMgr.h src\mui\MuiHwndWrapper.h src\mui\MuiLayout.h
 $(OMUI)\Mui.obj: src\mui\MuiPainter.h src\mui\MuiScrollBar.h src\utils\Allocator.h

mupdf/fitz/fitz.h
@@ -880,7 +880,7 @@ void fz_seek(fz_stream *stm, int offset, int whence);
 int fz_read(fz_stream *stm, unsigned char *buf, int len);
 void fz_read_line(fz_stream *stm, char *buf, int max);
 fz_buffer *fz_read_all(fz_stream *stm, int initial);
-/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692260 */
+/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=1587 */
 fz_buffer *fz_read_all2(fz_stream *stm, int initial, int fail_on_error);
 
 static inline int fz_read_byte(fz_stream *stm)

mupdf/fitz/stm_read.c
@@ -87,7 +87,7 @@ fz_fill_buffer(fz_stream *stm)
 	}
 }
 
-/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692260 */
+/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=1587 */
 fz_buffer *
 fz_read_all2(fz_stream *stm, int initial, int fail_on_error)
 {
@@ -112,7 +112,7 @@ fz_read_all2(fz_stream *stm, int initial, int fail_on_error)
 			if (buf->len / 200 > initial)
 				fz_throw(ctx, "compression bomb detected");
 
-			/* http://bugs.ghostscript.com/show_bug.cgi?id=692260 */
+			/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=1587 */
 			fz_try(ctx)
 			{
 				n = fz_read(stm, buf->data + buf->len, buf->cap - buf->len);

mupdf/libmupdf.def
@@ -156,6 +156,7 @@ EXPORTS
 	fz_open_buffer
 	fz_open_memory
 	fz_close
+	fz_lock_stream
 	fz_clone_stream
 	fz_new_stream
 	fz_keep_stream
@@ -249,6 +250,7 @@ EXPORTS
 	fz_bound_path
 	fz_debug_path
 	fz_new_glyph_cache_context
+	fz_glyph_cache_keep
 	fz_free_glyph_cache_context
 	fz_render_ft_glyph
 	fz_render_t3_glyph
@@ -317,7 +319,6 @@ EXPORTS
 	fz_free_display_list
 	fz_new_list_device
 	fz_run_display_list
-	fz_accelerate
 	fz_decode_tile
 	fz_decode_indexed_tile
 	fz_unpack_tile
@@ -339,6 +340,7 @@ EXPORTS
 	fz_debug_outline_xml
 	fz_debug_outline
 	fz_free_outline
+	fz_open_document
 	fz_close_document
 	fz_needs_password
 	fz_authenticate_password

mupdf/pdf/pdf_stream.c
@@ -397,7 +397,8 @@ pdf_load_stream(pdf_document *xref, int num, int gen)
 
 	fz_try(ctx)
 	{
-		buf = fz_read_all(stm, len);
+		/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=1587 */
+		buf = fz_read_all2(stm, len, 0);
 	}
 	fz_always(ctx)
 	{

scripts/generate-libmupdf.def.py
@@ -68,7 +68,7 @@ EXPORTS
 """
 
 def main():
-	fitz_exports = generateExports("fitz/fitz.h", ["fz_accelerate_arch", "fz_open_document"])
+	fitz_exports = generateExports("fitz/fitz.h", ["fz_assert_lock_held", "fz_assert_lock_not_held", "fz_lock_debug_lock", "fz_lock_debug_unlock"])
 	mupdf_exports = generateExports("pdf/mupdf.h")
 	muxps_exports = generateExports("xps/muxps.h", ["xps_parse_solid_color_brush", "xps_debug_path"])
 	mucbz_exports = generateExports("cbz/mucbz.h")