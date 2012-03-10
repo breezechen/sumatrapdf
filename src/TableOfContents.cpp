breezechen/sumatrapdf

commit 43086c9066056e9206fbec0ff0a769f10c877acd

update the ToC tree also when it has the focus (fixes issue 1828)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5749 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/TableOfContents.cpp
@@ -265,8 +265,6 @@ void UpdateTocSelection(WindowInfo *win, int currPageNo)
 {
     if (!win->tocLoaded || !win->tocVisible)
         return;
-    if (GetFocus() == win->hwndTocTree)
-        return;
 
     HTREEITEM hRoot = TreeView_GetRoot(win->hwndTocTree);
     if (!hRoot)

commit 0f635affdab8431bebb2ad7b0713ca3ec9b3b5bb

fix minor regression from r5749

src/TableOfContents.cpp
@@ -135,10 +135,11 @@ static void RelayoutTocItem(LPNMTVCUSTOMDRAW ntvcd)
 class GoToTocLinkWorkItem : public UIThreadWorkItem
 {
     DocTocItem *tocItem;
+    HTREEITEM hItem;
 
 public:
-    GoToTocLinkWorkItem(WindowInfo *win, DocTocItem *tocItem) :
-        UIThreadWorkItem(win), tocItem(tocItem) { }
+    GoToTocLinkWorkItem(WindowInfo *win, DocTocItem *tocItem, HTREEITEM hItem) :
+        UIThreadWorkItem(win), tocItem(tocItem), hItem(hItem) { }
 
     virtual void Execute() {
         if (!WindowInfoStillValid(win) || !win->IsDocLoaded() || !tocItem)
@@ -148,6 +149,9 @@ public:
             win->linkHandler->GotoLink(tocItem->GetLink());
         else if (tocItem->pageNo)
             win->dm->GoToPage(tocItem->pageNo, 0, true);
+        // make sure that the tree item that the user selected
+        // isn't unselected in UpdateTocSelection right again
+        TreeView_SelectItem(win->hwndTocTree, hItem);
     }
 };
 
@@ -164,7 +168,8 @@ static void GoToTocLinkForTVItem(WindowInfo* win, HWND hTV, HTREEITEM hItem=NULL
     if (!tocItem || !win->IsDocLoaded())
         return;
     if ((allowExternal || tocItem->GetLink() && str::Eq(tocItem->GetLink()->GetDestType(), "ScrollTo")) || tocItem->pageNo) {
-        QueueWorkItem(new GoToTocLinkWorkItem(win, tocItem));
+        // delay changing the page until the tree messages have been handled
+        QueueWorkItem(new GoToTocLinkWorkItem(win, tocItem, hItem));
     }
 }
 