breezechen/sumatrapdf

commit 61037ee53219f820b728beb9f962415c0aed5c2f

prevent empty selections (fixes crash 976007)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@6073 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/Selection.cpp
@@ -44,6 +44,10 @@ Vec<SelectionOnPage> *SelectionOnPage::FromRectangle(DisplayModel *dm, RectI rec
     }
     sel->Reverse();
 
+    if (sel->Count() == 0) {
+        delete sel;
+        return NULL;
+    }
     return sel;
 }
 
@@ -51,10 +55,16 @@ Vec<SelectionOnPage> *SelectionOnPage::FromTextSelect(TextSel *textSel)
 {
     Vec<SelectionOnPage> *sel = new Vec<SelectionOnPage>(textSel->len);
 
-    for (int i = textSel->len - 1; i >= 0; i--)
-        sel->Append(SelectionOnPage(textSel->pages[i], &textSel->rects[i].Convert<double>()));
+    for (int i = textSel->len - 1; i >= 0; i--) {
+        RectD rect = textSel->rects[i].Convert<double>();
+        sel->Append(SelectionOnPage(textSel->pages[i], &rect));
+    }
     sel->Reverse();
 
+    if (sel->Count() == 0) {
+        delete sel;
+        return NULL;
+    }
     return sel;
 }
 
@@ -139,7 +149,7 @@ void UpdateTextSelection(WindowInfo *win, bool select)
 
     DeleteOldSelectionInfo(win);
     win->selectionOnPage = SelectionOnPage::FromTextSelect(&win->dm->textSelection->result);
-    win->showSelection = true;
+    win->showSelection = win->selectionOnPage != NULL;
 }
 
 void ZoomToSelection(WindowInfo *win, float factor, bool relative)
@@ -266,14 +276,15 @@ void OnSelectAll(WindowInfo *win, bool textOnly)
         win->dm->textSelection->SelectUpTo(pageNo, -1);
         win->selectionRect = RectI::FromXY(INT_MIN / 2, INT_MIN / 2, INT_MAX, INT_MAX);
         UpdateTextSelection(win);
+        win->showSelection = true;
     }
     else {
         DeleteOldSelectionInfo(win, true);
         win->selectionRect = RectI::FromXY(INT_MIN / 2, INT_MIN / 2, INT_MAX, INT_MAX);
         win->selectionOnPage = SelectionOnPage::FromRectangle(win->dm, win->selectionRect);
+        win->showSelection = win->selectionOnPage != NULL;
     }
 
-    win->showSelection = true;
     win->RepaintAsync();
 }
 
@@ -343,7 +354,9 @@ void OnSelectionStop(WindowInfo *win, int x, int y, bool aborted)
     win->selectionRect = RectI::FromXY(win->selectionRect.x, win->selectionRect.y, x, y);
     if (aborted || (MA_SELECTING == win->mouseAction ? win->selectionRect.IsEmpty() : !win->selectionOnPage))
         DeleteOldSelectionInfo(win, true);
-    else if (win->mouseAction == MA_SELECTING)
+    else if (win->mouseAction == MA_SELECTING) {
         win->selectionOnPage = SelectionOnPage::FromRectangle(win->dm, win->selectionRect);
+        win->showSelection = win->selectionOnPage != NULL;
+    }
     win->RepaintAsync();
 }

src/SumatraPDF.cpp
@@ -2133,7 +2133,7 @@ static void OnMouseLeftButtonDblClk(WindowInfo& win, int x, int y, WPARAM key)
 
         DeleteOldSelectionInfo(&win, true);
         win.selectionOnPage = SelectionOnPage::FromRectangle(win.dm, rc);
-        win.showSelection = true;
+        win.showSelection = win.selectionOnPage != NULL;
         win.RepaintAsync();
     }
     delete pageEl;

commit 35dd9d73da1f9fd77ce1cd43669717d10c5fe059

fix selection regression from r6073

src/Selection.cpp
@@ -106,7 +106,7 @@ void PaintSelection(WindowInfo *win, HDC hdc)
     Vec<RectI> rects;
 
     if (win->mouseAction == MA_SELECTING) {
-        // during selecting
+        // during rectangle selection
         RectI selRect = win->selectionRect;
         if (selRect.dx < 0) {
             selRect.x += selRect.dx;
@@ -119,14 +119,22 @@ void PaintSelection(WindowInfo *win, HDC hdc)
 
         rects.Append(selRect);
     } else {
-        if (MA_SELECTING_TEXT == win->mouseAction)
+        // during text selection or after selection is done
+        if (MA_SELECTING_TEXT == win->mouseAction) {
             UpdateTextSelection(win);
+            if (!win->selectionOnPage) {
+                // prevent the selection from disappearing while the
+                // user is still at it (OnSelectionStop removes it
+                // if it is still empty at the end)
+                win->selectionOnPage = new Vec<SelectionOnPage>();
+                win->showSelection = true;
+            }
+        }
 
-        assert(win->selectionOnPage);
+        CrashIf(!win->selectionOnPage);
         if (!win->selectionOnPage)
             return;
 
-        // after selection is done
         for (size_t i = 0; i < win->selectionOnPage->Count(); i++)
             rects.Append(win->selectionOnPage->At(i).GetRect(win->dm));
     }
@@ -276,15 +284,14 @@ void OnSelectAll(WindowInfo *win, bool textOnly)
         win->dm->textSelection->SelectUpTo(pageNo, -1);
         win->selectionRect = RectI::FromXY(INT_MIN / 2, INT_MIN / 2, INT_MAX, INT_MAX);
         UpdateTextSelection(win);
-        win->showSelection = true;
     }
     else {
         DeleteOldSelectionInfo(win, true);
         win->selectionRect = RectI::FromXY(INT_MIN / 2, INT_MIN / 2, INT_MAX, INT_MAX);
         win->selectionOnPage = SelectionOnPage::FromRectangle(win->dm, win->selectionRect);
-        win->showSelection = win->selectionOnPage != NULL;
     }
 
+    win->showSelection = win->selectionOnPage != NULL;
     win->RepaintAsync();
 }
 