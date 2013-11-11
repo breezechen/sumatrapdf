breezechen/sumatrapdf

commit 25fe3054c71c8d023a004db3484429423ad4a81d

merge latest MuPDF update
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@8343 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/source/fitz/draw-glyph.c
@@ -27,7 +27,7 @@ struct fz_glyph_cache_entry_s
 	fz_glyph_cache_entry *lru_next;
 	fz_glyph_cache_entry *bucket_next;
 	fz_glyph_cache_entry *bucket_prev;
-	fz_pixmap *val;
+	fz_glyph *val;
 };
 
 struct fz_glyph_cache_s
@@ -68,7 +68,7 @@ drop_glyph_cache_entry(fz_context *ctx, fz_glyph_cache_entry *entry)
 		entry->lru_prev->lru_next = entry->lru_next;
 	else
 		cache->lru_head = entry->lru_next;
-	cache->total -= entry->val->w * entry->val->h;
+	cache->total -= fz_glyph_size(ctx, entry->val);
 	if (entry->bucket_next)
 		entry->bucket_next->bucket_prev = entry->bucket_prev;
 	if (entry->bucket_prev)
@@ -76,7 +76,7 @@ drop_glyph_cache_entry(fz_context *ctx, fz_glyph_cache_entry *entry)
 	else
 		cache->entry[entry->hash] = entry->bucket_next;
 	fz_drop_font(ctx, entry->key.font);
-	fz_drop_pixmap(ctx, entry->val);
+	fz_drop_glyph(ctx, entry->val);
 	fz_free(ctx, entry);
 }
 
@@ -130,16 +130,78 @@ fz_keep_glyph_cache(fz_context *ctx)
 	return ctx->glyph_cache;
 }
 
-fz_pixmap *
-fz_render_stroked_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *trm, const fz_matrix *ctm, fz_stroke_state *stroke, fz_irect scissor)
+float
+fz_subpixel_adjust(fz_matrix *ctm, fz_matrix *subpix_ctm, unsigned char *qe, unsigned char *qf)
+{
+	float size = fz_matrix_expansion(ctm);
+	int q;
+	float pix_e, pix_f, r;
+
+	/* Quantise the subpixel positions. */
+	/* We never need more than 4 subpixel positions for glyphs - arguably
+	 * even that is too much. */
+	if (size >= 48)
+		q = 0, r = 0.5f;
+	else if (size >= 24)
+		q = 128, r = 0.25f;
+	else
+		q = 192, r = 0.125f;
+
+	/* Split translation into pixel and subpixel parts */
+	subpix_ctm->a = ctm->a;
+	subpix_ctm->b = ctm->b;
+	subpix_ctm->c = ctm->c;
+	subpix_ctm->d = ctm->d;
+	subpix_ctm->e = ctm->e + r;
+	pix_e = floorf(subpix_ctm->e);
+	subpix_ctm->e -= pix_e;
+	subpix_ctm->f = ctm->f + r;
+	pix_f = floorf(subpix_ctm->f);
+	subpix_ctm->f -= pix_f;
+
+	/* Quantise the subpixel part */
+	*qe = (int)(subpix_ctm->e * 256) & q;
+	subpix_ctm->e = *qe / 256.0f;
+	*qf = (int)(subpix_ctm->f * 256) & q;
+	subpix_ctm->f = *qf / 256.0f;
+
+	/* Reassemble the complete translation */
+	ctm->e = subpix_ctm->e + pix_e;
+	ctm->f = subpix_ctm->f + pix_f;
+
+	return size;
+}
+
+fz_glyph *
+fz_render_stroked_glyph(fz_context *ctx, fz_font *font, int gid, fz_matrix *trm, const fz_matrix *ctm, fz_stroke_state *stroke, const fz_irect *scissor)
 {
 	if (font->ft_face)
 	{
+		fz_matrix subpix_trm;
+		unsigned char qe, qf;
+
 		if (stroke->dash_len > 0)
 			return NULL;
-		return fz_render_ft_stroked_glyph(ctx, font, gid, trm, ctm, stroke);
+		(void)fz_subpixel_adjust(trm, &subpix_trm, &qe, &qf);
+		return fz_render_ft_stroked_glyph(ctx, font, gid, &subpix_trm, ctm, stroke);
 	}
 	return fz_render_glyph(ctx, font, gid, trm, NULL, scissor);
+}
+
+fz_pixmap *
+fz_render_stroked_glyph_pixmap(fz_context *ctx, fz_font *font, int gid, fz_matrix *trm, const fz_matrix *ctm, fz_stroke_state *stroke, const fz_irect *scissor)
+{
+	if (font->ft_face)
+	{
+		fz_matrix subpix_trm;
+		unsigned char qe, qf;
+
+		if (stroke->dash_len > 0)
+			return NULL;
+		(void)fz_subpixel_adjust(trm, &subpix_trm, &qe, &qf);
+		return fz_render_ft_stroked_glyph_pixmap(ctx, font, gid, &subpix_trm, ctm, stroke);
+	}
+	return fz_render_glyph_pixmap(ctx, font, gid, trm, NULL, scissor);
 }
 
 static unsigned do_hash(unsigned char *s, int len)
@@ -178,33 +240,26 @@ move_to_front(fz_glyph_cache *cache, fz_glyph_cache_entry *entry)
 	entry->lru_prev = NULL;
 }
 
-/*
-	Render a glyph and return a bitmap.
-	If the glyph is too large to fit the cache we have two choices:
-	1) Return NULL so the caller can draw the glyph using an outline.
-		Only supported for freetype fonts.
-	2) Render a clipped glyph by using the scissor rectangle.
-		Only supported for type 3 fonts.
-		This must not be inserted into the cache.
- */
-fz_pixmap *
-fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, fz_colorspace *model, fz_irect scissor)
+fz_glyph *
+fz_render_glyph(fz_context *ctx, fz_font *font, int gid, fz_matrix *ctm, fz_colorspace *model, const fz_irect *scissor)
 {
 	fz_glyph_cache *cache;
 	fz_glyph_key key;
-	fz_pixmap *val;
-	float size = fz_matrix_expansion(ctm);
+	fz_matrix subpix_ctm;
+	float size;
+	fz_glyph *val;
 	int do_cache, locked, caching;
-	fz_matrix local_ctm = *ctm;
 	fz_glyph_cache_entry *entry;
 	unsigned hash;
 
 	fz_var(locked);
 	fz_var(caching);
 
+	memset(&key, 0, sizeof key);
+	size = fz_subpixel_adjust(ctm, &subpix_ctm, &key.e, &key.f);
 	if (size <= MAX_GLYPH_SIZE)
 	{
-		scissor = fz_infinite_irect;
+		scissor = &fz_infinite_irect;
 		do_cache = 1;
 	}
 	else
@@ -217,19 +272,13 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, f
 
 	cache = ctx->glyph_cache;
 
-	memset(&key, 0, sizeof key);
 	key.font = font;
 	key.gid = gid;
-	key.a = local_ctm.a * 65536;
-	key.b = local_ctm.b * 65536;
-	key.c = local_ctm.c * 65536;
-	key.d = local_ctm.d * 65536;
-	key.e = (local_ctm.e - floorf(local_ctm.e)) * 256;
-	key.f = (local_ctm.f - floorf(local_ctm.f)) * 256;
+	key.a = subpix_ctm.a * 65536;
+	key.b = subpix_ctm.b * 65536;
+	key.c = subpix_ctm.c * 65536;
+	key.d = subpix_ctm.d * 65536;
 	key.aa = fz_aa_level(ctx);
-
-	local_ctm.e = floorf(local_ctm.e) + key.e / 256.0f;
-	local_ctm.f = floorf(local_ctm.f) + key.f / 256.0f;
 
 	fz_lock(ctx, FZ_LOCK_GLYPHCACHE);
 	hash = do_hash((unsigned char *)&key, sizeof(key)) % GLYPH_HASH_LEN;
@@ -239,7 +288,7 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, f
 		if (memcmp(&entry->key, &key, sizeof(key)) == 0)
 		{
 			move_to_front(cache, entry);
-			val = fz_keep_pixmap(ctx, entry->val);
+			val = fz_keep_glyph(ctx, entry->val);
 			fz_unlock(ctx, FZ_LOCK_GLYPHCACHE);
 			return val;
 		}
@@ -253,7 +302,7 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, f
 	{
 		if (font->ft_face)
 		{
-			val = fz_render_ft_glyph(ctx, font, gid, &local_ctm, key.aa);
+			val = fz_render_ft_glyph(ctx, font, gid, &subpix_ctm, key.aa);
 		}
 		else if (font->t3procs)
 		{
@@ -268,7 +317,7 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, f
 			 */
 			fz_unlock(ctx, FZ_LOCK_GLYPHCACHE);
 			locked = 0;
-			val = fz_render_t3_glyph(ctx, font, gid, &local_ctm, model, scissor);
+			val = fz_render_t3_glyph(ctx, font, gid, &subpix_ctm, model, scissor);
 			fz_lock(ctx, FZ_LOCK_GLYPHCACHE);
 			locked = 1;
 		}
@@ -293,9 +342,9 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, f
 					{
 						if (memcmp(&entry->key, &key, sizeof(key)) == 0)
 						{
-							fz_drop_pixmap(ctx, val);
+							fz_drop_glyph(ctx, val);
 							move_to_front(cache, entry);
-							val = fz_keep_pixmap(ctx, entry->val);
+							val = fz_keep_glyph(ctx, entry->val);
 							fz_unlock(ctx, FZ_LOCK_GLYPHCACHE);
 							return val;
 						}
@@ -310,7 +359,7 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, f
 				if (entry->bucket_next)
 					entry->bucket_next->bucket_prev = entry;
 				cache->entry[hash] = entry;
-				entry->val = fz_keep_pixmap(ctx, val);
+				entry->val = fz_keep_glyph(ctx, val);
 				fz_keep_font(ctx, key.font);
 
 				entry->lru_next = cache->lru_head;
@@ -320,12 +369,12 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, f
 					cache->lru_tail = entry;
 				cache->lru_head = entry;
 
-				cache->total += val->w * val->h;
+				cache->total += fz_glyph_size(ctx, val);
 				while (cache->total > MAX_CACHE_SIZE)
 				{
 #ifndef NDEBUG
 					cache->num_evictions++;
-					cache->evicted += cache->lru_tail->val->w * cache->lru_tail->val->h;
+					cache->evicted += fz_glyph_size(ctx, cache->lru_tail->val);
 #endif
 					drop_glyph_cache_entry(ctx, cache->lru_tail);
 				}
@@ -344,6 +393,48 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm, f
 			fz_warn(ctx, "cannot encache glyph; continuing");
 		else
 			fz_rethrow(ctx);
+	}
+
+	return val;
+}
+
+fz_pixmap *
+fz_render_glyph_pixmap(fz_context *ctx, fz_font *font, int gid, fz_matrix *ctm, fz_colorspace *model, const fz_irect *scissor)
+{
+	fz_pixmap *val;
+	unsigned char qe, qf;
+	fz_matrix subpix_ctm;
+	float size = fz_subpixel_adjust(ctm, &subpix_ctm, &qe, &qf);
+
+	if (size <= MAX_GLYPH_SIZE)
+	{
+		scissor = &fz_infinite_irect;
+	}
+	else
+	{
+		if (font->ft_face)
+			return NULL;
+	}
+
+	fz_try(ctx)
+	{
+		if (font->ft_face)
+		{
+			val = fz_render_ft_glyph_pixmap(ctx, font, gid, &subpix_ctm, fz_aa_level(ctx));
+		}
+		else if (font->t3procs)
+		{
+			val = fz_render_t3_glyph_pixmap(ctx, font, gid, &subpix_ctm, model, scissor);
+		}
+		else
+		{
+			fz_warn(ctx, "assert: uninitialized font structure");
+			val = NULL;
+		}
+	}
+	fz_catch(ctx)
+	{
+		fz_rethrow(ctx);
 	}
 
 	return val;

makefile.deps
@@ -340,25 +340,25 @@ $(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\buffer.h $B\mupdf\include\mupdf
 $(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\context.h $B\mupdf\include\mupdf\fitz\crypt.h $B\mupdf\include\mupdf\fitz\device.h
 $(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\display-list.h $B\mupdf\include\mupdf\fitz\document.h $B\mupdf\include\mupdf\fitz\filter.h
 $(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\font.h $B\mupdf\include\mupdf\fitz\function.h $B\mupdf\include\mupdf\fitz\getopt.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\glyph-cache.h $B\mupdf\include\mupdf\fitz\hash.h $B\mupdf\include\mupdf\fitz\image.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\link.h $B\mupdf\include\mupdf\fitz\math.h $B\mupdf\include\mupdf\fitz\meta.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\outline.h $B\mupdf\include\mupdf\fitz\output-pcl.h $B\mupdf\include\mupdf\fitz\output-png.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\output-pnm.h $B\mupdf\include\mupdf\fitz\output-pwg.h $B\mupdf\include\mupdf\fitz\output-svg.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\output-tga.h $B\mupdf\include\mupdf\fitz\output.h $B\mupdf\include\mupdf\fitz\path.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\pixmap.h $B\mupdf\include\mupdf\fitz\shade.h $B\mupdf\include\mupdf\fitz\store.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\stream.h $B\mupdf\include\mupdf\fitz\string.h $B\mupdf\include\mupdf\fitz\structured-text.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\system.h $B\mupdf\include\mupdf\fitz\text.h $B\mupdf\include\mupdf\fitz\transition.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\write-document.h $B\mupdf\include\mupdf\fitz\xml.h $B\mupdf\include\mupdf\pdf.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\annot.h $B\mupdf\include\mupdf\pdf\appearance.h $B\mupdf\include\mupdf\pdf\cmap.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\crypt.h $B\mupdf\include\mupdf\pdf\document.h $B\mupdf\include\mupdf\pdf\event.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\field.h $B\mupdf\include\mupdf\pdf\font.h $B\mupdf\include\mupdf\pdf\javascript.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\object.h $B\mupdf\include\mupdf\pdf\output-pdf.h $B\mupdf\include\mupdf\pdf\page.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\parse.h $B\mupdf\include\mupdf\pdf\resource.h $B\mupdf\include\mupdf\pdf\widget.h
-$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\xref.h $B\mupdf\include\mupdf\xps.h $B\src\BaseEngine.h
-$(OS)\PdfEngine.obj: $B\src\PdfEngine.h $B\src\utils\Allocator.h $B\src\utils\BaseUtil.h
-$(OS)\PdfEngine.obj: $B\src\utils\FileUtil.h $B\src\utils\GeomUtil.h $B\src\utils\Scoped.h
-$(OS)\PdfEngine.obj: $B\src\utils\StrUtil.h $B\src\utils\Vec.h $B\src\utils\WinUtil.h
-$(OS)\PdfEngine.obj: $B\src\utils\ZipUtil.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\glyph-cache.h $B\mupdf\include\mupdf\fitz\glyph.h $B\mupdf\include\mupdf\fitz\hash.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\image.h $B\mupdf\include\mupdf\fitz\link.h $B\mupdf\include\mupdf\fitz\math.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\meta.h $B\mupdf\include\mupdf\fitz\outline.h $B\mupdf\include\mupdf\fitz\output-pcl.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\output-png.h $B\mupdf\include\mupdf\fitz\output-pnm.h $B\mupdf\include\mupdf\fitz\output-pwg.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\output-svg.h $B\mupdf\include\mupdf\fitz\output-tga.h $B\mupdf\include\mupdf\fitz\output.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\path.h $B\mupdf\include\mupdf\fitz\pixmap.h $B\mupdf\include\mupdf\fitz\shade.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\store.h $B\mupdf\include\mupdf\fitz\stream.h $B\mupdf\include\mupdf\fitz\string.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\structured-text.h $B\mupdf\include\mupdf\fitz\system.h $B\mupdf\include\mupdf\fitz\text.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\fitz\transition.h $B\mupdf\include\mupdf\fitz\write-document.h $B\mupdf\include\mupdf\fitz\xml.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf.h $B\mupdf\include\mupdf\pdf\annot.h $B\mupdf\include\mupdf\pdf\appearance.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\cmap.h $B\mupdf\include\mupdf\pdf\crypt.h $B\mupdf\include\mupdf\pdf\document.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\event.h $B\mupdf\include\mupdf\pdf\field.h $B\mupdf\include\mupdf\pdf\font.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\javascript.h $B\mupdf\include\mupdf\pdf\object.h $B\mupdf\include\mupdf\pdf\output-pdf.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\page.h $B\mupdf\include\mupdf\pdf\parse.h $B\mupdf\include\mupdf\pdf\resource.h
+$(OS)\PdfEngine.obj: $B\mupdf\include\mupdf\pdf\widget.h $B\mupdf\include\mupdf\pdf\xref.h $B\mupdf\include\mupdf\xps.h
+$(OS)\PdfEngine.obj: $B\src\BaseEngine.h $B\src\PdfEngine.h $B\src\utils\Allocator.h
+$(OS)\PdfEngine.obj: $B\src\utils\BaseUtil.h $B\src\utils\FileUtil.h $B\src\utils\GeomUtil.h
+$(OS)\PdfEngine.obj: $B\src\utils\Scoped.h $B\src\utils\StrUtil.h $B\src\utils\Vec.h
+$(OS)\PdfEngine.obj: $B\src\utils\WinUtil.h $B\src\utils\ZipUtil.h
 $(OS)\PdfSync.obj: $B\src\BaseEngine.h $B\src\PdfEngine.h $B\src\PdfSync.h
 $(OS)\PdfSync.obj: $B\src\utils\Allocator.h $B\src\utils\BaseUtil.h $B\src\utils\FileUtil.h
 $(OS)\PdfSync.obj: $B\src\utils\GeomUtil.h $B\src\utils\Scoped.h $B\src\utils\StrUtil.h

mupdf/include/mupdf/fitz.h
@@ -22,6 +22,7 @@
 #include "mupdf/fitz/store.h"
 #include "mupdf/fitz/colorspace.h"
 #include "mupdf/fitz/pixmap.h"
+#include "mupdf/fitz/glyph.h"
 #include "mupdf/fitz/bitmap.h"
 #include "mupdf/fitz/image.h"
 #include "mupdf/fitz/function.h"

mupdf/include/mupdf/fitz/glyph-cache.h
@@ -20,13 +20,19 @@ void fz_purge_glyph_cache(fz_context *ctx);
 
 fz_path *fz_outline_ft_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *trm);
 fz_path *fz_outline_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *ctm);
-fz_pixmap *fz_render_ft_glyph(fz_context *ctx, fz_font *font, int cid, const fz_matrix *trm, int aa);
-fz_pixmap *fz_render_t3_glyph(fz_context *ctx, fz_font *font, int cid, const fz_matrix *trm, fz_colorspace *model, fz_irect scissor);
-fz_pixmap *fz_render_ft_stroked_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *trm, const fz_matrix *ctm, fz_stroke_state *state);
-fz_pixmap *fz_render_glyph(fz_context *ctx, fz_font*, int, const fz_matrix *, fz_colorspace *model, fz_irect scissor);
-fz_pixmap *fz_render_stroked_glyph(fz_context *ctx, fz_font*, int, const fz_matrix *, const fz_matrix *, fz_stroke_state *stroke, fz_irect scissor);
+fz_glyph *fz_render_ft_glyph(fz_context *ctx, fz_font *font, int cid, const fz_matrix *trm, int aa);
+fz_pixmap *fz_render_ft_glyph_pixmap(fz_context *ctx, fz_font *font, int cid, const fz_matrix *trm, int aa);
+fz_glyph *fz_render_t3_glyph(fz_context *ctx, fz_font *font, int cid, const fz_matrix *trm, fz_colorspace *model, const fz_irect *scissor);
+fz_pixmap *fz_render_t3_glyph_pixmap(fz_context *ctx, fz_font *font, int cid, const fz_matrix *trm, fz_colorspace *model, const fz_irect *scissor);
+fz_glyph *fz_render_ft_stroked_glyph(fz_context *ctx, fz_font *font, int gid, const fz_matrix *trm, const fz_matrix *ctm, fz_stroke_state *state);
+fz_pixmap *fz_render_ft_stroked_glyph_pixmap(fz_context *ctx, fz_font *font, int gid, const fz_matrix *trm, const fz_matrix *ctm, fz_stroke_state *state);
+fz_glyph *fz_render_glyph(fz_context *ctx, fz_font*, int, fz_matrix *, fz_colorspace *model, const fz_irect *scissor);
+fz_pixmap *fz_render_glyph_pixmap(fz_context *ctx, fz_font*, int, fz_matrix *, fz_colorspace *model, const fz_irect *scissor);
+fz_glyph *fz_render_stroked_glyph(fz_context *ctx, fz_font*, int, fz_matrix *, const fz_matrix *, fz_stroke_state *stroke, const fz_irect *scissor);
+fz_pixmap *fz_render_stroked_glyph_pixmap(fz_context *ctx, fz_font*, int, fz_matrix *, const fz_matrix *, fz_stroke_state *stroke, const fz_irect *scissor);
 void fz_render_t3_glyph_direct(fz_context *ctx, fz_device *dev, fz_font *font, int gid, const fz_matrix *trm, void *gstate, int nestedDepth);
 void fz_prepare_t3_glyph(fz_context *ctx, fz_font *font, int gid, int nestedDepth);
 void fz_dump_glyph_cache_stats(fz_context *ctx);
+float fz_subpixel_adjust(fz_matrix *ctm, fz_matrix *subpix_ctm, unsigned char *qe, unsigned char *qf);
 
 #endif

mupdf/include/mupdf/fitz/glyph.h
@@ -1,0 +1,137 @@
+#ifndef MUPDF_FITZ_GLYPH_H
+#define MUPDF_FITZ_GLYPH_H
+
+#include "mupdf/fitz/system.h"
+#include "mupdf/fitz/context.h"
+#include "mupdf/fitz/math.h"
+#include "mupdf/fitz/store.h"
+#include "mupdf/fitz/colorspace.h"
+
+/*
+	Glyphs represent a run length encoded set of pixels for a 2
+	dimensional region of a plane.
+*/
+typedef struct fz_glyph_s fz_glyph;
+
+/*
+	fz_glyph_bbox: Return the bounding box for a glyph.
+*/
+fz_irect *fz_glyph_bbox(fz_context *ctx, fz_glyph *glyph, fz_irect *bbox);
+
+/*
+	fz_glyph_width: Return the width of the glyph in pixels.
+*/
+int fz_glyph_width(fz_context *ctx, fz_glyph *glyph);
+
+/*
+	fz_glyph_height: Return the height of the glyph in pixels.
+*/
+int fz_glyph_height(fz_context *ctx, fz_glyph *glyph);
+
+/*
+	fz_new_glyph_from_pixmap: Create a new glyph from a pixmap
+
+	Returns a pointer to the new glyph. Throws exception on failure to
+	allocate.
+*/
+fz_glyph *fz_new_glyph_from_pixmap(fz_context *ctx, fz_pixmap *pix);
+
+/*
+	fz_new_glyph_from_8bpp_data: Create a new glyph from 8bpp data
+
+	x, y: X and Y position for the glyph
+
+	w, h: Width and Height for the glyph
+
+	sp: Source Pointer to data
+
+	span: Increment from line to line of data
+
+	Returns a pointer to the new glyph. Throws exception on failure to
+	allocate.
+*/
+fz_glyph *fz_new_glyph_from_8bpp_data(fz_context *ctx, int x, int y, int w, int h, unsigned char *sp, int span);
+
+/*
+	fz_new_glyph_from_1bpp_data: Create a new glyph from 1bpp data
+
+	x, y: X and Y position for the glyph
+
+	w, h: Width and Height for the glyph
+
+	sp: Source Pointer to data
+
+	span: Increment from line to line of data
+
+	Returns a pointer to the new glyph. Throws exception on failure to
+	allocate.
+*/fz_glyph *fz_new_glyph_from_1bpp_data(fz_context *ctx, int x, int y, int w, int h, unsigned char *sp, int span);
+
+
+/*
+	fz_keep_glyph: Take a reference to a glyph.
+
+	pix: The glyph to increment the reference for.
+
+	Returns pix. Does not throw exceptions.
+*/
+fz_glyph *fz_keep_glyph(fz_context *ctx, fz_glyph *pix);
+
+/*
+	fz_drop_glyph: Drop a reference and free a glyph.
+
+	Decrement the reference count for the glyph. When no
+	references remain the glyph will be freed.
+
+	Does not throw exceptions.
+*/
+void fz_drop_glyph(fz_context *ctx, fz_glyph *pix);
+
+/*
+	Glyphs represent a set of pixels for a 2 dimensional region of a
+	plane.
+
+	x, y: The minimum x and y coord of the region in pixels.
+
+	w, h: The width and height of the region in pixels.
+
+	samples: The sample data. The sample data is in a compressed format
+	designed to give reasonable compression, and to be fast to plot from.
+
+	The first sizeof(int) * h bytes of the table, when interpreted as
+	ints gives the offset within the data block of that lines data. An
+	offset of 0 indicates that that line is completely blank.
+
+	The data for individual lines is a sequence of bytes:
+	 00000000 = end of lines data
+	 LLLLLL00 = extend the length given in the next run by the 6 L bits
+		given here.
+	 LLLLLL01 = A run of length L+1 transparent pixels.
+	 LLLLLE10 = A run of length L+1 solid pixels. If E then this is the
+		last run on this line.
+	 LLLLLE11 = A run of length L+1 intermediate pixels followed by L+1
+		bytes of literal pixel data. If E then this is the last run
+		on this line.
+*/
+struct fz_glyph_s
+{
+	fz_storable storable;
+	int x, y, w, h;
+	fz_pixmap *pixmap;
+	int size;
+	unsigned char data[1];
+};
+
+static unsigned int fz_glyph_size(fz_context *ctx, fz_glyph *glyph);
+
+fz_irect *fz_glyph_bbox_no_ctx(fz_glyph *src, fz_irect *bbox);
+
+static inline unsigned int
+fz_glyph_size(fz_context *ctx, fz_glyph *glyph)
+{
+	if (glyph == NULL)
+		return 0;
+	return sizeof(fz_glyph) + glyph->size + fz_pixmap_size(ctx, glyph->pixmap);
+}
+
+#endif

mupdf/include/mupdf/fitz/output-png.h
@@ -27,4 +27,12 @@ void fz_output_png(fz_output *out, const fz_pixmap *pixmap, int savealpha);
 */
 fz_buffer *fz_image_as_png(fz_context *ctx, fz_image *image, int w, int h);
 
+typedef struct fz_png_output_context_s fz_png_output_context;
+
+fz_png_output_context *fz_output_png_header(fz_output *out, int w, int h, int n, int savealpha);
+
+void fz_output_png_band(fz_output *out, int w, int h, int n, int band, int bandheight, unsigned char *samples, int savealpha, fz_png_output_context *poc);
+
+void fz_output_png_trailer(fz_output *out, fz_png_output_context *poc);
+
 #endif

mupdf/include/mupdf/fitz/output-pnm.h
@@ -14,12 +14,18 @@
 */
 void fz_write_pnm(fz_context *ctx, fz_pixmap *pixmap, char *filename);
 
+void fz_output_pnm_header(fz_output *out, int w, int h, int n);
+void fz_output_pnm_band(fz_output *out, int w, int h, int n, int band, int bandheight, unsigned char *p);
+
 /*
 	fz_write_pam: Save a pixmap as a pam
 
 	filename: The filename to save as (including extension).
 */
 void fz_write_pam(fz_context *ctx, fz_pixmap *pixmap, char *filename, int savealpha);
+
+void fz_output_pam_header(fz_output *out, int w, int h, int n, fz_colorspace *cs, int savealpha);
+void fz_output_pam_band(fz_output *out, int w, int h, int n, int band, int bandheight, unsigned char *sp, int savealpha);
 
 /*
 	fz_write_pbm: Save a bitmap as a pbm

mupdf/include/mupdf/fitz/output.h
@@ -28,6 +28,11 @@ struct fz_output_s
 fz_output *fz_new_output_with_file(fz_context *, FILE *);
 
 /*
+	fz_new_output_to_filename: Open an output stream to a filename.
+*/
+fz_output *fz_new_output_to_filename(fz_context *, const char *filename);
+
+/*
 	fz_new_output_with_buffer: Open an output stream onto a buffer.
 
 	The stream does NOT take ownership of the buffer.
@@ -48,6 +53,11 @@ int fz_puts(fz_output *, const char *);
 	fz_write: fwrite equivalent for output streams.
 */
 int fz_write(fz_output *out, const void *data, int len);
+
+/*
+	fz_putc: putc equivalent for output streams.
+*/
+void fz_putc(fz_output *out, char c);
 
 /*
 	fz_close_output: Close a previously opened fz_output stream.

commit 7b5f46153c7940b004d9bcaeee89e2191970691a

fix Type 3 glyph clipping bug introduced with r8343 (fixes issue 2466)

mupdf/source/fitz/draw-glyph.c
@@ -251,6 +251,7 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, fz_matrix *ctm, fz_colo
 	int do_cache, locked, caching;
 	fz_glyph_cache_entry *entry;
 	unsigned hash;
+	fz_irect subpix_scissor; /* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=2466 */
 
 	fz_var(locked);
 	fz_var(caching);
@@ -269,6 +270,12 @@ fz_render_glyph(fz_context *ctx, fz_font *font, int gid, fz_matrix *ctm, fz_colo
 		if (font->ft_face && size > 3000)
 			return NULL;
 		do_cache = 0;
+		/* cf. http://code.google.com/p/sumatrapdf/issues/detail?id=2466 */
+		subpix_scissor.x0 = scissor->x0 - floorf(ctm->e);
+		subpix_scissor.y0 = scissor->y0 - floorf(ctm->f);
+		subpix_scissor.x1 = scissor->x1 - floorf(ctm->e);
+		subpix_scissor.y1 = scissor->y1 - floorf(ctm->f);
+		scissor = &subpix_scissor;
 	}
 
 	cache = ctx->glyph_cache;