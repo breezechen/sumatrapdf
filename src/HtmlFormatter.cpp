breezechen/sumatrapdf

commit a3f79a3ecd74b391f7badba8917caad6a9eca0f3

fix remaining compilation warnings under x64
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@8505 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/HtmlFormatter.cpp
@@ -60,7 +60,7 @@ Control objects further to make allocating hundreds of them cheaper or introduce
 other base element(s) with less functionality and less overhead).
 */
 
-bool ValidReparseIdx(int idx, HtmlPullParser *parser)
+bool ValidReparseIdx(ptrdiff_t idx, HtmlPullParser *parser)
 {
     if ((idx < 0) || (idx > (int)parser->Len()))
         return false;
@@ -533,7 +533,8 @@ bool HtmlFormatter::FlushCurrLine(bool isParagraphBreak)
         // so we have to carry over some state (like current font)
         CrashIf(!CurrFont());
         EmitNewPage();
-        currPage->reparseIdx = currLineReparseIdx;
+        CrashIf(currLineReparseIdx > INT_MAX);
+        currPage->reparseIdx = (int)currLineReparseIdx;
         createdPage = true;
     }
     SetYPos(currLineInstr, currY + currLineTopPadding);
@@ -560,7 +561,8 @@ bool HtmlFormatter::FlushCurrLine(bool isParagraphBreak)
 
 void HtmlFormatter::EmitNewPage()
 {
-    currPage = new HtmlPage(currReparseIdx);
+    CrashIf(currReparseIdx > INT_MAX);
+    currPage = new HtmlPage((int)currReparseIdx);
     currPage->instructions.Append(DrawInstr::SetFont(nextPageStyle.font));
     currY = 0.f;
 }
@@ -728,10 +730,10 @@ void HtmlFormatter::EmitTextRun(const char *s, const char *end)
             break;
         }
 
-        int lenThatFits = StringLenForWidth(gfx, CurrFont(), buf, strLen, pageDx - NewLineX(), measureAlgo);
+        size_t lenThatFits = StringLenForWidth(gfx, CurrFont(), buf, strLen, pageDx - NewLineX(), measureAlgo);
         // try to prevent a break in the middle of a word
         if (iswalnum(buf[lenThatFits])) {
-            for (int len = lenThatFits; len > 0; len--) {
+            for (size_t len = lenThatFits; len > 0; len--) {
                 if (!iswalnum(buf[len-1])) {
                     lenThatFits = len;
                     break;
@@ -743,7 +745,7 @@ void HtmlFormatter::EmitTextRun(const char *s, const char *end)
         // s is UTF-8 and buf is UTF-16, so one
         // WCHAR doesn't always equal one char
         // TODO: this usually fails for non-BMP characters (i.e. hardly ever)
-        for (int i = lenThatFits - 1; i >= 0; i--) {
+        for (size_t i = lenThatFits - 1; i >= 0; i--) {
             lenThatFits += buf[i] < 0x80 ? 0 : buf[i] < 0x800 ? 1 : 2;
         }
         AppendInstr(DrawInstr::Str(s, lenThatFits, bbox, dirRtl));
@@ -1359,7 +1361,7 @@ void DrawHtmlPage(Graphics *g, Vec<DrawInstr> *drawInstructions, REAL offX, REAL
                 g->DrawRectangle(&debugPen, bbox);
             g->DrawLine(&linePen, p1, p2);
         } else if (InstrString == i->type) {
-            size_t strLen = str::Utf8ToWcharBuf(i->str.s, i->str.len, buf, dimof(buf));
+            int strLen = (int)str::Utf8ToWcharBuf(i->str.s, i->str.len, buf, dimof(buf));
             bbox.GetLocation(&pos);
             if (showBbox)
                 g->DrawRectangle(&debugPen, bbox);
@@ -1386,7 +1388,7 @@ void DrawHtmlPage(Graphics *g, Vec<DrawInstr> *drawInstructions, REAL offX, REAL
         } else if (InstrLinkEnd == i->type) {
             // TODO: set text color back again
         } else if (InstrRtlString == i->type) {
-            size_t strLen = str::Utf8ToWcharBuf(i->str.s, i->str.len, buf, dimof(buf));
+            int strLen = (int)str::Utf8ToWcharBuf(i->str.s, i->str.len, buf, dimof(buf));
             bbox.GetLocation(&pos);
             if (showBbox)
                 g->DrawRectangle(&debugPen, bbox);

ext/_patches/jbig2dec.patch
@@ -1,42 +1,0 @@
-diff -rPu5 jbig2dec.orig\jbig2_symbol_dict.c jbig2dec\jbig2_symbol_dict.c
---- jbig2dec.orig\jbig2_symbol_dict.c	Sat Dec 14 16:00:01 2013
-+++ jbig2dec\jbig2_symbol_dict.c	Sat Dec 14 18:43:36 2013
-@@ -155,10 +155,11 @@
- 
-     for (index = 0; index < segment->referred_to_segment_count; index++) {
-         rsegment = jbig2_find_segment(ctx, segment->referred_to_segments[index]);
-         if (rsegment && ((rsegment->flags & 63) == 0) &&
-             rsegment->result &&
-+            (((Jbig2SymbolDict *)rsegment->result)->n_symbols > 0) &&
-             ((*((Jbig2SymbolDict *)rsegment->result)->glyphs) != NULL))
-             n_dicts++;
-     }
- 
-     return (n_dicts);
-@@ -1047,23 +1048,13 @@
-   params.SDNUMEXSYMS = jbig2_get_uint32(segment_data + offset);
-   /* 7.4.2.1.5 */
-   params.SDNUMNEWSYMS = jbig2_get_uint32(segment_data + offset + 4);
-   offset += 8;
- 
--  if (params.SDNUMEXSYMS == 0 || params.SDNUMNEWSYMS == 0)
--  {
--    jbig2_error(ctx, JBIG2_SEVERITY_FATAL, segment->number,
--      "empty symbol dictionary, flags=%04x, %u exported syms, %u new syms",
--      flags, params.SDNUMEXSYMS, params.SDNUMNEWSYMS);
--    goto cleanup;
--  }
--  else
--  {
--    jbig2_error(ctx, JBIG2_SEVERITY_INFO, segment->number,
--      "symbol dictionary, flags=%04x, %u exported syms, %u new syms",
--      flags, params.SDNUMEXSYMS, params.SDNUMNEWSYMS);
--  }
-+  jbig2_error(ctx, JBIG2_SEVERITY_INFO, segment->number,
-+    "symbol dictionary, flags=%04x, %u exported syms, %u new syms",
-+    flags, params.SDNUMEXSYMS, params.SDNUMNEWSYMS);
- 
-   /* 7.4.2.2 (2) */
-   {
-     int n_dicts = jbig2_sd_count_referred(ctx, segment);
-     Jbig2SymbolDict **dicts = NULL;

ext/jbig2dec/jbig2_symbol_dict.c
@@ -1051,8 +1051,8 @@ jbig2_symbol_dictionary(Jbig2Ctx *ctx, Jbig2Segment *segment,
   offset += 8;
 
   jbig2_error(ctx, JBIG2_SEVERITY_INFO, segment->number,
-    "symbol dictionary, flags=%04x, %u exported syms, %u new syms",
-    flags, params.SDNUMEXSYMS, params.SDNUMNEWSYMS);
+	      "symbol dictionary, flags=%04x, %u exported syms, %u new syms",
+	      flags, params.SDNUMEXSYMS, params.SDNUMNEWSYMS);
 
   /* 7.4.2.2 (2) */
   {

ext/makefile.msvc
@@ -43,6 +43,9 @@ CHMLIB_CFLAGS = $(CFLAGS) /TC /D "CHM_MT"
 CHMLIB_CFLAGS = $(CHMLIB_CFLAGS) /wd4018 /wd4057 /wd4189 /wd4244 /wd4295 /wd4701 /wd4706 /wd4996
 # ChmLib also needs PPC_BSTR defined for UNICODE builds
 CHMLIB_CFLAGS = $(CHMLIB_CFLAGS) /D "UNICODE" /D "_UNICODE" /D "PPC_BSTR"
+!if "$(PLATFORM)"=="X64"
+CHMLIB_CFLAGS = $(CHMLIB_CFLAGS) /wd4267
+!endif
 
 LZMA_CFLAGS = $(CFLAGSOPT) /TC /I$(EXTDIR)/lzma/C
 

ext/zlib/infback9/inftree9.c
@@ -256,7 +256,7 @@ unsigned short FAR *work;
                 drop = root;
 
             /* increment past last table */
-            next += 1U << curr;
+            next += (size_t)1 << curr; /* SumatraPDF: prevent C4334 */
 
             /* determine length of next table */
             curr = len - drop;

mupdf/makefile.msvc
@@ -98,7 +98,8 @@ CFLAGSB = $(CFLAGSB) /W4
 CFLAGSB = $(CFLAGSB) /wd4100 /wd4127 /wd4189
 # warnings that might not make sense to fix:
 # 4428 : universal-character-name encountered in source (prevents using "\u202A" etc.)
-CFLAGSB = $(CFLAGSB) /wd4428
+# 4324 : structure was padded due to __declspec(align())
+CFLAGSB = $(CFLAGSB) /wd4428 /wd4324
 
 # /Ox  : maximum optimizations
 # /O2  : maximize speed

mupdf/source/fitz/gdiplus-device.cpp
@@ -31,7 +31,7 @@ static LONG g_gdiplusUsage = 0;
 static ColorPalette *
 gdiplus_create_grayscale_palette(fz_context *ctx, int depth)
 {
-	ColorPalette * palette = (ColorPalette *)fz_malloc(ctx, sizeof(ColorPalette) + (1 << depth) * sizeof(ARGB));
+	ColorPalette * palette = (ColorPalette *)fz_malloc(ctx, sizeof(ColorPalette) + ((size_t)1 << depth) * sizeof(ARGB));
 	
 	palette->Flags = PaletteFlagsGrayScale;
 	palette->Count = 1 << depth;

src/AppPrefs.cpp
@@ -387,8 +387,8 @@ bool Reload(bool forceReload)
     UpdateDocumentColors();
     UpdateFavoritesTreeForAllWindows();
 
-    int n = gEbookWindows.Count();
-    for (int i=0; i < n; i++) {
+    size_t n = gEbookWindows.Count();
+    for (size_t i = 0; i < n; i++) {
         EbookWindowRefreshUI(gEbookWindows.At(i));
     }
 

commit 0956ca73d40f4637b846a7774f38a1e870f6e940

fix crash introduced with r8505

src/HtmlFormatter.cpp
@@ -745,8 +745,8 @@ void HtmlFormatter::EmitTextRun(const char *s, const char *end)
         // s is UTF-8 and buf is UTF-16, so one
         // WCHAR doesn't always equal one char
         // TODO: this usually fails for non-BMP characters (i.e. hardly ever)
-        for (size_t i = lenThatFits - 1; i >= 0; i--) {
-            lenThatFits += buf[i] < 0x80 ? 0 : buf[i] < 0x800 ? 1 : 2;
+        for (size_t i = lenThatFits; i > 0; i--) {
+            lenThatFits += buf[i-1] < 0x80 ? 0 : buf[i-1] < 0x800 ? 1 : 2;
         }
         AppendInstr(DrawInstr::Str(s, lenThatFits, bbox, dirRtl));
         currX += bbox.Width;