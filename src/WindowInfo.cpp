breezechen/sumatrapdf

commit 6e5d7a1bc2bd74ae371c7066e2837ee1f25b0659

remove fragment identifier from URI when opening as local file
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@8502 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/WindowInfo.cpp
@@ -233,14 +233,20 @@ void LinkHandler::GotoLink(PageDestination *link)
     else if (Dest_LaunchURL == type) {
         if (!path)
             /* ignore missing URLs */;
-        else if (!str::FindChar(path, ':')) {
-            // treat relative URIs as file paths
-            // LaunchFile will reject unsupported file types
-            LaunchFile(path, NULL);
-        }
         else {
-            // LaunchBrowser will reject unsupported URI schemes
-            LaunchBrowser(path);
+            WCHAR *colon = str::FindChar(path, ':');
+            WCHAR *hash = str::FindChar(path, '#');
+            if (!colon || colon > hash) {
+                // treat relative URIs as file paths (without fragment identifier)
+                if (hash)
+                    *hash = '\0';
+                // LaunchFile will reject unsupported file types
+                LaunchFile(path, NULL);
+            }
+            else {
+                // LaunchBrowser will reject unsupported URI schemes
+                LaunchBrowser(path);
+            }
         }
     }
     else if (Dest_LaunchEmbedded == type) {

src/PdfEngine.cpp
@@ -3024,10 +3024,11 @@ int PdfEngineImpl::GetPageByLabel(const WCHAR *label) const
 
 static bool IsRelativeURI(const WCHAR *uri)
 {
-    const WCHAR *colon = str::FindChar(uri, ':');
-    const WCHAR *slash = str::FindChar(uri, '/');
-
-    return !colon || (slash && colon > slash);
+    const WCHAR *c = uri;
+    while (*c && *c != ':' && *c != '/' && *c != '?' && *c != '#') {
+        c++;
+    }
+    return *c != ':';
 }
 
 WCHAR *PdfLink::GetValue() const

commit 857c14d5be5410b04c7d23c9847e1af78a51619c

fix URI loading broken with r8502

src/WindowInfo.cpp
@@ -236,7 +236,7 @@ void LinkHandler::GotoLink(PageDestination *link)
         else {
             WCHAR *colon = str::FindChar(path, ':');
             WCHAR *hash = str::FindChar(path, '#');
-            if (!colon || colon > hash) {
+            if (!colon || (hash && colon > hash)) {
                 // treat relative URIs as file paths (without fragment identifier)
                 if (hash)
                     *hash = '\0';